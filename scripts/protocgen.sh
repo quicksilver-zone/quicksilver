c7c6111ee1be082a2874f8e3530cf47bfde399b1
Joe Bowman <joe@ingenuity.build>
2022-02-17 00:38:30 +0000

    add basic CI/CD and RocksDB for performance at epochs

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -eox pipefail
 
 protoc_gen_gocosmos() {
   if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
@@ -8,12 +8,12 @@ protoc_gen_gocosmos() {
     return 1
   fi
 
-  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
+  go install github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 }
 
 protoc_gen_doc() {
-  go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc 2>/dev/null
-} 
+  echo 'raa' #go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc 2>/dev/null
+}
 
 protoc_gen_gocosmos
 protoc_gen_doc
