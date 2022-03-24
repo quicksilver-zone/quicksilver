040da13d17392f2b60b5fc021648c0984113da39
Joe Bowman <joe@ingenuity.build>
2022-03-24 22:23:26 +0000

    render swagger docs

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 mkdir -p ./tmp-swagger-gen
 proto_dirs=$(find ./proto ./third_party/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-    
+
     # generate swagger files (filter query files)
     query_file=$(find "${dir}" -maxdepth 1 -name 'query.proto')
     if [[ ! -z "$query_file" ]]; then
