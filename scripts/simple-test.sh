ab30383334b1af2078dc916e59bb854346511fab
Joe Bowman <joe@ingenuity.build>
2022-04-27 22:22:22 +0100

    tweaks for simple test case setup for icq callbacks

--- scripts/simple-test.sh
+++ scripts/simple-test.sh
@@ -19,7 +19,7 @@ else
   rm -rf ${CHAIN_DIR}/hermes &> /dev/null
   rm -rf ${CHAIN_DIR}/icq &> /dev/null
 
-  TIME=$(date --date '-1 minute' +%Y-%m-%dT%H:%m:%S.%NZ -u)
+  TIME=$(date --date '-2 minutes' +%Y-%m-%dT%H:%m:%S.%NZ -u)
   jq ".genesis_time = \"$TIME\"" ./${CHAIN_DIR}/backup/${CHAINID_1}/config/genesis.json > ./${CHAIN_DIR}/backup/${CHAINID_1}/config/genesis.json.new && mv ./${CHAIN_DIR}/backup/${CHAINID_1}/config/genesis.json{.new,}
 
   cp -fr ${CHAIN_DIR}/backup/${CHAINID_1} ${CHAIN_DIR}/${CHAINID_1}
