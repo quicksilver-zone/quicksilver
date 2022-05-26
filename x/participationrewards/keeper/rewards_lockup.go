730e4a778a1a9973c8675c240e36f495b376f30f
Zanicar <zanicar@noctranet.com>
2022-05-26 08:39:47 +0200

    allocateValidatorChoiceRewards implementation wip
    - contextualized devtest output
    - test zone added for keeper test
    
    Blocked on validator performance scoring;

--- x/participationrewards/keeper/rewards_lockup.go
+++ x/participationrewards/keeper/rewards_lockup.go
@@ -10,11 +10,12 @@ import (
 func (k Keeper) allocateLockupRewards(ctx sdk.Context, allocation sdk.Coins) error {
 	k.Logger(ctx).Info("allocateLockupRewards", "allocation", allocation)
 	// DEVTEST:
-	fmt.Printf("\t\tAllocate Lockup Rewards:\t\t%v\n", allocation)
+	if ctx.Context().Value("DEVTEST") == "DEVTEST" {
+		fmt.Printf("\t\tAllocate Lockup Rewards:\t\t%v\n", allocation)
+	}
 
 	// allocate staking incentives into fee collector account to be moved to on next begin blocker by staking module
-	err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, k.feeCollectorName, allocation)
-	if err != nil {
+	if err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, k.feeCollectorName, allocation); err != nil {
 		return err
 	}
 
