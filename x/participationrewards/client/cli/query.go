d034c230f6a0c4db7e1a8a57791d80fd9ae9c089
Zanicar <zanicar@noctranet.com>
2022-06-03 14:35:29 +0200

    participation-rewards-module: minor updates
    - added performance account rewards withdrawals
    - cleaned up cli/query.go

--- x/participationrewards/client/cli/query.go
+++ x/participationrewards/client/cli/query.go
@@ -1,58 +1,26 @@
 package cli
 
 import (
-	"context"
+	"fmt"
 
 	"github.com/spf13/cobra"
 
 	"github.com/ingenuity-build/quicksilver/x/mint/types"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
 )
 
 // GetQueryCmd returns the cli query commands for the minting module.
 func GetQueryCmd() *cobra.Command {
-	mintingQueryCmd := &cobra.Command{
+	cmd := &cobra.Command{
 		Use:                        types.ModuleName,
-		Short:                      "Querying commands for the minting module",
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
 		DisableFlagParsing:         true,
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
 
-	mintingQueryCmd.AddCommand(
-		GetCmdQueryParams(),
-	)
-
-	return mintingQueryCmd
-}
-
-// GetCmdQueryParams implements a command to return the current minting
-// parameters.
-func GetCmdQueryParams() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "params",
-		Short: "Query the current minting parameters",
-		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx, err := client.GetClientQueryContext(cmd)
-			if err != nil {
-				return err
-			}
-			queryClient := types.NewQueryClient(clientCtx)
-
-			params := &types.QueryParamsRequest{}
-			res, err := queryClient.Params(context.Background(), params)
-			if err != nil {
-				return err
-			}
-
-			return clientCtx.PrintProto(&res.Params)
-		},
-	}
-
-	flags.AddQueryFlagsToCmd(cmd)
+	cmd.AddCommand()
 
 	return cmd
 }
