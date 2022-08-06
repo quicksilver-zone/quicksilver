75ab0bc0670c119387a34164e2bc068846977bf0
Joe Bowman <joe@ingenuity.build>
2022-08-06 11:17:24 +0100

    fix error msg

--- x/airdrop/types/errors.go
+++ x/airdrop/types/errors.go
@@ -13,7 +13,7 @@ var (
 	ErrUnknownStatus        = sdkerrors.Register(ModuleName, 3, "unknown status")
 	ErrUndefinedAttribute   = sdkerrors.Register(ModuleName, 4, "expected attribute not defined")
 	ErrInvalidDuration      = sdkerrors.Register(ModuleName, 5, "invalid duration")
-	ErrActionOutOfBounds    = sdkerrors.Register(ModuleName, 6, fmt.Sprintf("invalid action, expects range [0-%d)", len(Action_value)))
+	ErrActionOutOfBounds    = sdkerrors.Register(ModuleName, 6, fmt.Sprintf("invalid action, expects range [0-%d]", len(Action_value)))
 	ErrDuplicateZoneDrop    = sdkerrors.Register(ModuleName, 7, "duplicate zone drop")
 	ErrDuplicateClaimRecord = sdkerrors.Register(ModuleName, 8, "duplicate claim record")
 	ErrAllocationExceeded   = sdkerrors.Register(ModuleName, 9, "claim records allocations exceed zone drop allocation")
