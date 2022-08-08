43dd958c349937bd98365f212eb37a3bd1bf8d23
Zanicar <zanicar@noctranet.com>
2022-08-08 15:16:29 +0200

    participation-rewards: defer distribution
     - defer initial rewards distribution until 3rd epoch;

--- x/participationrewards/keeper/rewards_lockup.go
+++ x/participationrewards/keeper/rewards_lockup.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/ingenuity-build/quicksilver/x/participationrewards/types"
@@ -10,10 +8,6 @@ import (
 
 func (k Keeper) allocateLockupRewards(ctx sdk.Context, allocation sdk.Coins) error {
 	k.Logger(ctx).Info("allocateLockupRewards", "allocation", allocation)
-	// DEVTEST:
-	if ctx.Context().Value("DEVTEST") == "DEVTEST" {
-		fmt.Printf("\t\tAllocate Lockup Rewards:\t\t%v\n", allocation)
-	}
 
 	// allocate staking incentives into fee collector account to be moved to on next begin blocker by staking module
 	if err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleName, k.feeCollectorName, allocation); err != nil {
