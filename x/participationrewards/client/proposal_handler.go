c1b5ae3ead36975f9c6cb1b25b9be8d5a23aa994
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2022-08-13 15:42:56 +0700

    change proto of ibc to v5

--- x/participationrewards/client/proposal_handler.go
+++ x/participationrewards/client/proposal_handler.go
@@ -1,25 +1,11 @@
 package client
 
 import (
-	"net/http"
-
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/types/rest"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
-	govrest "github.com/cosmos/cosmos-sdk/x/gov/client/rest"
 	"github.com/ingenuity-build/quicksilver/x/participationrewards/client/cli"
 )
 
 // ProposalHandler is the community spend proposal handler.
 var (
-	AddProtocolDataProposalHandler = govclient.NewProposalHandler(cli.GetCmdAddProtocolDataProposal, emptyRestHandler)
+	AddProtocolDataProposalHandler = govclient.NewProposalHandler(cli.GetCmdAddProtocolDataProposal)
 )
-
-func emptyRestHandler(client.Context) govrest.ProposalRESTHandler {
-	return govrest.ProposalRESTHandler{
-		SubRoute: "unsupported-client",
-		Handler: func(w http.ResponseWriter, r *http.Request) {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, "Legacy REST Routes are not supported for PR proposals")
-		},
-	}
-}
