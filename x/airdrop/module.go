d1105384b4083a8bbe47c5405ad8d42959f6df6d
Joe Bowman <joe@ingenuity.build>
2022-08-29 17:40:20 -0500

    ensure msgserver registered in airdrop

--- x/airdrop/module.go
+++ x/airdrop/module.go
@@ -137,6 +137,7 @@ func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sd
 // RegisterServices registers a gRPC query service to respond to the
 // module-specific gRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
+	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 }
 
