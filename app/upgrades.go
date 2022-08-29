95f1605bae943e7314b302a8e4a71103ff4e6026
Joe Bowman <joe@ingenuity.build>
2022-08-29 16:50:33 -0500

    add upgrade handler for v0.6.2

---
+++ app/upgrades.go
@@ -0,0 +1,46 @@
+package app
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	airdroptypes "github.com/ingenuity-build/quicksilver/x/airdrop/types"
+)
+
+func GetInnuendo1Upgrade(app *Quicksilver) types.UpgradeHandler {
+
+	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		ReplaceZoneDropChain(ctx, app, "osmotest-4", "osmo-test-4", ctx.BlockHeader().Time)
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	}
+}
+
+// replaces zonedrop and claimrecords for a given chain, with another chain and update start time.
+func ReplaceZoneDropChain(ctx sdk.Context, app *Quicksilver, chainIdFrom string, chainIdTo string, start time.Time) {
+	ad, found := app.AirdropKeeper.GetZoneDrop(ctx, chainIdFrom)
+	if !found {
+		panic(chainIdFrom + " zonedrop not found")
+	}
+	// update chainid for osmo-test-4 airdrop and reset start time.
+	ad.ChainId = chainIdTo
+	ad.StartTime = start
+
+	app.AirdropKeeper.SetZoneDrop(ctx, ad)
+	app.AirdropKeeper.IterateClaimRecords(ctx, chainIdFrom, func(index int64, cr airdroptypes.ClaimRecord) (stop bool) {
+		ctx.Logger().Info("migrating claimdrop record", "address", cr.Address)
+		cr.ChainId = chainIdTo
+		app.AirdropKeeper.SetClaimRecord(ctx, cr)
+		app.AirdropKeeper.DeleteClaimRecord(ctx, chainIdFrom, cr.Address)
+		return false
+	})
+
+	app.AirdropKeeper.DeleteZoneDrop(ctx, chainIdFrom)
+
+	// update unbonding time to 48h.
+	stakeParams := app.StakingKeeper.GetParams(ctx)
+	stakeParams.UnbondingTime = 48 * time.Hour
+	app.StakingKeeper.SetParams(ctx, stakeParams)
+}
---
+++ app/upgrades_test.go
@@ -0,0 +1,62 @@
+package app
+
+import (
+	"testing"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	airdroptypes "github.com/ingenuity-build/quicksilver/x/airdrop/types"
+	"github.com/stretchr/testify/require"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+func TestInnuendo1Upgrade(t *testing.T) {
+	// set up zone drop record and claims.
+	app := Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+	zd := airdroptypes.ZoneDrop{
+		ChainId:    "osmotest-4",
+		StartTime:  time.Now().AddDate(0, 0, -1),
+		Duration:   time.Hour,
+		Decay:      time.Hour,
+		Allocation: 1000000,
+		Actions: []sdk.Dec{
+			sdk.OneDec(),
+		},
+		IsConcluded: false,
+	}
+
+	app.AirdropKeeper.SetZoneDrop(ctx, zd)
+
+	claim1 := airdroptypes.ClaimRecord{
+		ChainId:          "osmotest-4",
+		Address:          "quick1g035r8sl346ttxuj0555yxdwftr52t849t3q39",
+		ActionsCompleted: make(map[int32]*airdroptypes.CompletedAction),
+		MaxAllocation:    500000,
+		BaseValue:        500000,
+	}
+
+	claim2 := airdroptypes.ClaimRecord{
+		ChainId:          "osmotest-4",
+		Address:          "quick1u53f8u6jjdpxquesk8tqxzv9hvqx7qyfzlkdrj",
+		ActionsCompleted: make(map[int32]*airdroptypes.CompletedAction),
+		MaxAllocation:    500000,
+		BaseValue:        500000,
+	}
+
+	err := app.AirdropKeeper.SetClaimRecord(ctx, claim1)
+	require.NoError(t, err)
+	err = app.AirdropKeeper.SetClaimRecord(ctx, claim2)
+	require.NoError(t, err)
+	claims := app.AirdropKeeper.AllZoneClaimRecords(ctx, "osmotest-4")
+	require.Equal(t, 2, len(claims))
+	require.NotPanics(t, func() { ReplaceZoneDropChain(ctx, app, "osmotest-4", "osmo-test-4", ctx.BlockHeader().Time) })
+	claimsAfter := app.AirdropKeeper.AllZoneClaimRecords(ctx, "osmotest-4")
+	require.Equal(t, 0, len(claimsAfter))
+	claimsNew := app.AirdropKeeper.AllZoneClaimRecords(ctx, "osmo-test-4")
+	require.Equal(t, 2, len(claimsNew))
+	zoneDropsAfter := app.AirdropKeeper.AllZoneDrops(ctx)
+	require.Equal(t, 1, len(zoneDropsAfter))
+	require.Equal(t, zoneDropsAfter[0].ChainId, "osmo-test-4")
+	require.Equal(t, zoneDropsAfter[0].StartTime, ctx.BlockHeader().Time)
+}
