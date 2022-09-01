39baf9e5a6f37756da3ea8712574634a535870c8
Zanicar <38106253+zanicar@users.noreply.github.com>
2022-09-01 13:36:57 +0200

    remediation: audit updates & fixes (#234)
    - refactored SubmitQueryResponse;
    - updated duplicate query handling;
    - now correctly handles query deletion and updates;

--- x/interchainquery/keeper/msg_server.go
+++ x/interchainquery/keeper/msg_server.go
@@ -26,61 +26,72 @@ var _ types.MsgServer = msgServer{}
 func (k msgServer) SubmitQueryResponse(goCtx context.Context, msg *types.MsgSubmitQueryResponse) (*types.MsgSubmitQueryResponseResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	q, found := k.GetQuery(ctx, msg.QueryId)
-	// if found && q.LastHeight.Int64() != ctx.BlockHeader().Height {
-	if found {
-		pathParts := strings.Split(q.QueryType, "/")
-		if pathParts[len(pathParts)-1] == "key" {
-			if err := utils.ValidateProofOps(ctx, k.IBCKeeper, q.ConnectionId, q.ChainId, msg.Height, pathParts[1], q.Request, msg.Result, msg.ProofOps); err != nil {
-				return nil, err
-			}
+
+	if !found {
+		k.Logger(ctx).Info("query not found", "QueryID", msg.QueryId)
+
+		return &types.MsgSubmitQueryResponseResponse{}, nil
+	}
+
+	// check if query was previously processed
+	// - indicated by query.LastHeight matching current Block Height;
+	if q.LastHeight.Int64() == ctx.BlockHeader().Height {
+		k.Logger(ctx).Info("ignoring duplicate query")
+		// technically this is an error, but will cause the entire tx to fail
+		// if we have one 'bad' message, so we can just no-op here.
+		return &types.MsgSubmitQueryResponseResponse{}, nil
+	}
+
+	pathParts := strings.Split(q.QueryType, "/")
+	if pathParts[len(pathParts)-1] == "key" {
+		if err := utils.ValidateProofOps(ctx, k.IBCKeeper, q.ConnectionId, q.ChainId, msg.Height, pathParts[1], q.Request, msg.Result, msg.ProofOps); err != nil {
+			return nil, err
 		}
+	}
 
-		noDelete := false
-		// execute registered callbacks.
+	noDelete := false
+	// execute registered callbacks.
 
-		keys := []string{}
-		for k := range k.callbacks {
-			keys = append(keys, k)
-		}
+	keys := []string{}
+	for k := range k.callbacks {
+		keys = append(keys, k)
+	}
 
-		sort.Strings(keys)
+	sort.Strings(keys)
 
-		for _, key := range keys {
-			module := k.callbacks[key]
-			if module.Has(q.CallbackId) {
-				err := module.Call(ctx, q.CallbackId, msg.Result, q)
-				if err != nil {
-					// handle edge case; callback has resent the same query!
-					// set noDelete to true and short circuit error handling!
-					if err == types.ErrSucceededNoDelete {
-						noDelete = true
-					} else {
-						k.Logger(ctx).Error("error in callback", "error", err, "msg", msg.QueryId, "result", msg.Result, "type", q.QueryType, "params", q.Request)
-						return nil, err
-					}
+	for _, key := range keys {
+		module := k.callbacks[key]
+		if module.Has(q.CallbackId) {
+			err := module.Call(ctx, q.CallbackId, msg.Result, q)
+			if err != nil {
+				// not edge case: proceed with regular error handling!
+				if err != types.ErrSucceededNoDelete {
+					k.Logger(ctx).Error("error in callback", "error", err, "msg", msg.QueryId, "result", msg.Result, "type", q.QueryType, "params", q.Request)
+					return nil, err
 				}
+				// edge case: the callback has resent the same query (re-query)!
+				// action:    set noDelete to true and continue (short circuit error handling)!
+				noDelete = true
 			}
 		}
+	}
 
-		if q.Ttl > 0 {
-			// don't store if ttl is 0
-			if err := k.SetDatapointForID(ctx, msg.QueryId, msg.Result, sdk.NewInt(msg.Height)); err != nil {
-				return nil, err
-			}
+	if q.Ttl > 0 {
+		// don't store if ttl is 0
+		if err := k.SetDatapointForID(ctx, msg.QueryId, msg.Result, sdk.NewInt(msg.Height)); err != nil {
+			return nil, err
 		}
+	}
 
-		if q.Period.IsNegative() {
-			if !noDelete {
-				k.DeleteQuery(ctx, msg.QueryId)
-			}
-		} else {
-			q.LastHeight = sdk.NewInt(ctx.BlockHeight())
-			k.SetQuery(ctx, q)
-		}
-
+	// check for and delete non-repeating queries, update any other
+	// - Period.IsNegative() indicates a single query;
+	// - noDelete indicates a response that triggered a re-query;
+	if q.Period.IsNegative() && !noDelete {
+		k.DeleteQuery(ctx, msg.QueryId)
 	} else {
-		k.Logger(ctx).Info("Ignoring duplicate query")
-		return &types.MsgSubmitQueryResponseResponse{}, nil // technically this is an error, but will cause the entire tx to fail if we have one 'bad' message, so we can just no-op here.
+		// logic condition: !q.Period.IsNegative() || noDelete == true
+		q.LastHeight = sdk.NewInt(ctx.BlockHeight())
+		k.SetQuery(ctx, q)
 	}
 
 	ctx.EventManager().EmitEvents(sdk.Events{
