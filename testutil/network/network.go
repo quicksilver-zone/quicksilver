d4f2ee6f91a09864032adcc9e83a35e9c7939e65
Jacob Gadikian <jacobgadikian@gmail.com>
2022-09-20 22:51:16 +0700

    more token factory

---
+++ testutil/network/network.go
@@ -0,0 +1,75 @@
+package network
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/ingenuity-build/quicksilver/app"
+	appparams "github.com/ingenuity-build/quicksilver/app/params"
+
+	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	tmdb "github.com/tendermint/tm-db"
+)
+
+type (
+	Network = network.Network
+	Config  = network.Config
+)
+
+// New creates instance with fully configured cosmos network.
+// Accepts optional config, that will be used in place of the DefaultConfig() if provided.
+func New(t *testing.T, configs ...network.Config) *network.Network {
+	if len(configs) > 1 {
+		panic("at most one config should be provided")
+	}
+	var cfg network.Config
+	if len(configs) == 0 {
+		cfg = DefaultConfig()
+	} else {
+		cfg = configs[0]
+	}
+	net, err := network.New(nil, "", cfg) // TODO:
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(net.Cleanup)
+	return net
+}
+
+// DefaultConfig will initialize config for the network with custom application,
+// genesis and single validator. All other parameters are inherited from cosmos-sdk/testutil/network.DefaultConfig
+func DefaultConfig() network.Config {
+	encoding := appparams.MakeEncodingConfig(app.ModuleBasics)
+	return network.Config{
+		Codec:             encoding.Codec,
+		TxConfig:          encoding.TxConfig,
+		LegacyAmino:       encoding.Amino,
+		InterfaceRegistry: encoding.InterfaceRegistry,
+		AccountRetriever:  authtypes.AccountRetriever{},
+		AppConstructor: func(val network.Validator) servertypes.Application {
+			return app.NewQuicksilver(
+				val.Ctx.Logger, tmdb.NewMemDB(), nil, true, map[int64]bool{}, val.Ctx.Config.RootDir, 0,
+				encoding,
+				app.GetEnabledProposals(),
+				app.EmptyAppOptions{},
+				nil,
+				// baseapp.SetPruning(types.NewPruningOptionsFromString(val.AppConfig.Pruning)),
+				// baseapp.SetMinGasPrices(val.AppConfig.MinGasPrices),
+			)
+		},
+		// GenesisState:    app.ModuleBasics.DefaultGenesis(encoding.Marshaler),
+		// TimeoutCommit:   2 * time.Second,
+		// ChainID:         "chain-" + tmrand.NewRand().Str(6),
+		// NumValidators:   1,
+		// BondDenom:       sdk.DefaultBondDenom,
+		// MinGasPrices:    fmt.Sprintf("0.000006%s", sdk.DefaultBondDenom),
+		// AccountTokens:   sdk.TokensFromConsensusPower(1000, sdk.DefaultPowerReduction),
+		// StakingTokens:   sdk.TokensFromConsensusPower(500, sdk.DefaultPowerReduction),
+		// BondedTokens:    sdk.TokensFromConsensusPower(100, sdk.DefaultPowerReduction),
+		// PruningStrategy: types.PruningOptionNothing,
+		// CleanupDir:      true,
+		// SigningAlgo:     string(hd.Secp256k1Type),
+		// KeyringOptions:  []keyring.Option{},
+	}
+}
---
+++ testutil/simapp/simapp.go
@@ -0,0 +1,35 @@
+package simapp
+
+// New creates application instance with in-memory database and disabled logging.
+// func New(dir string) cosmoscmd.App {
+// 	db := tmdb.NewMemDB()
+// 	logger := log.NewNopLogger()
+
+// 	encoding := cosmoscmd.MakeEncodingConfig(app.ModuleBasics)
+
+// 	a := app.NewEveApp(logger, db, nil, true, map[int64]bool{}, dir, 0, encoding,
+// 		simapp.EmptyAppOptions{})
+// 	// InitChain updates deliverState which is required when app.NewContext is called
+// 	a.InitChain(abci.RequestInitChain{
+// 		ConsensusParams: defaultConsensusParams,
+// 		AppStateBytes:   []byte("{}"),
+// 	})
+// 	return a
+// }
+
+// var defaultConsensusParams = &abci.ConsensusParams{
+// 	Block: &abci.BlockParams{
+// 		MaxBytes: 200000,
+// 		MaxGas:   2000000,
+// 	},
+// 	Evidence: &tmproto.EvidenceParams{
+// 		MaxAgeNumBlocks: 302400,
+// 		MaxAgeDuration:  504 * time.Hour, // 3 weeks is the max duration
+// 		MaxBytes:        10000,
+// 	},
+// 	Validator: &tmproto.ValidatorParams{
+// 		PubKeyTypes: []string{
+// 			tmtypes.ABCIPubKeyTypeEd25519,
+// 		},
+// 	},
+// }
