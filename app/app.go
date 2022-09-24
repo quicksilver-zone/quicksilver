51b3dbd5f07341f5961995f92a53977ddf0d8adf
Jacob Gadikian <jacobgadikian@gmail.com>
2022-09-25 00:32:20 +0700

    Revert "remove osmosis features from wasm implementation"
    This reverts commit 259b9855db17c0622e34a1b0313081fdeb6eaddb.

--- app/app.go
+++ app/app.go
@@ -512,7 +512,7 @@ func NewQuicksilver(
 
 	// The last arguments can contain custom message handlers, and custom query handlers,
 	// if we want to allow any custom callbacks
-	supportedFeatures := "iterator,staking,stargate"
+	supportedFeatures := "iterator,staking,stargate,osmosis"
 	wasmOpts = append(wasmbinding.RegisterCustomPlugins(&app.BankKeeper, app.TokenFactoryKeeper), wasmOpts...)
 	wasmOpts = append(wasmbinding.RegisterStargateQueries(*bApp.GRPCQueryRouter(), appCodec), wasmOpts...)
 
