2ebf080bf0e5f60da53fc04e40020106c31d0e8c
Joe Bowman <joe@ingenuity.build>
2022-10-06 05:10:29 +0100

    wip; add cachemanager to avoid recurring lookups

--- xcc.go
+++ xcc.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/cosmos/btcutil/bech32"
@@ -13,32 +13,94 @@ import (
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/gorilla/mux"
+	osmolockup "github.com/ingenuity-build/quicksilver/osmosis-types/lockup"
 	prewards "github.com/ingenuity-build/quicksilver/x/participationrewards/types"
-	osmolockup "github.com/osmosis-labs/osmosis/v10/x/lockup/types"
-	tmcrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
 	tmhttp "github.com/tendermint/tendermint/rpc/client/http"
 	libclient "github.com/tendermint/tendermint/rpc/jsonrpc/client"
 )
 
+var connectionManager CacheManager[prewards.ConnectionProtocolData]
+var poolsManager CacheManager[prewards.OsmosisPoolProtocolData]
+
 type Response struct {
 	Messages []prewards.MsgSubmitClaim `json:"messages"`
+}
+
+type Data[T any] struct {
+	Data []T
+}
+
+type CacheManager[T any] struct {
+	url         string
+	cache       []T
+	lastUpdated time.Time
+}
+
+func (c *CacheManager[T]) Init(url string, updateTime time.Duration) {
+	c.url = url
+	c.Fetch()
+}
+
+func (c *CacheManager[T]) Fetch() {
+	response, err := http.Get(c.url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	fmt.Println(responseData)
+	if err != nil {
+		panic(err)
+	}
+
+	data := Data[T]{}
+
+	err = json.Unmarshal(responseData, &data)
+	if err != nil {
+		panic(err)
+	}
+	c.cache = data.Data
+}
+
+func (c CacheManager[T]) Get() []T {
+	if time.Now().After(c.lastUpdated.Add(time.Minute * 5)) {
+		c.Fetch()
+	}
+	return c.cache
 }
 
 func handle(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	var err error
+
 	response := Response{}
-	ts, err := strconv.ParseInt(vars["timestamp"], 10, 64)
-	if err != nil {
-		fmt.Fprintf(w, "Error: %s", err)
+	var height int64
+	connections := connectionManager.Get()
+	for _, con := range connections {
+		if con.ChainID == "osmosis-1" {
+			height = con.LastEpoch
+			break
+		}
+		if con.ChainID == "quickosmo-1" {
+			height = con.LastEpoch
+			break
+		}
+		if con.ChainID == "osmo-test-4" {
+			height = con.LastEpoch
+			break
+		}
+	}
+	if height == 0 {
 		return
 	}
-	// query epoch time
+	// liquid for all zones; config should hold osmosis chainid.
 
 	// query zones and last heights
 
-	response.Messages, err = handleOsmo(vars["address"], ts)
+	response.Messages, err = handleOsmo(vars["address"], height)
 	if err != nil {
 		fmt.Fprintf(w, "Error: %s", err)
 		return
@@ -54,7 +116,7 @@ func handle(w http.ResponseWriter, req *http.Request) {
 
 type poolMap map[string][]int64
 
-func handleOsmo(address string, timestamp int64) ([]prewards.MsgSubmitClaim, error) {
+func handleOsmo(address string, height int64) ([]prewards.MsgSubmitClaim, error) {
 	_, addrBytes, err := bech32.Decode(address, 51)
 	if err != nil {
 		return nil, err
@@ -65,16 +127,18 @@ func handleOsmo(address string, timestamp int64) ([]prewards.MsgSubmitClaim, err
 	}
 	fmt.Println("b")
 
-	client, err := NewRPCClient("https://rpc-osmosis.whispernode.com:443", 30*time.Second)
+	client, err := NewRPCClient("http://dev:23101", 30*time.Second)
 	if err != nil {
 		return nil, err
 	}
+	// fetch timestamp of block
+
 	query := osmolockup.AccountLockedPastTimeRequest{Owner: osmoAddress, Timestamp: time.Unix(timestamp, 0)}
 	interfaceRegistry := cdctypes.NewInterfaceRegistry()
 	osmolockup.RegisterInterfaces(interfaceRegistry)
 	marshaler := codec.NewProtoCodec(interfaceRegistry)
 	bytes := marshaler.MustMarshal(&query)
-
+	// how to pass header for height!
 	abciquery, err := client.ABCIQuery(context.Background(), "/osmosis.lockup.Query/AccountLockedPastTime", bytes)
 	if err != nil {
 		return nil, err
@@ -86,8 +150,18 @@ func handleOsmo(address string, timestamp int64) ([]prewards.MsgSubmitClaim, err
 		return nil, err
 	}
 
+	poolIds := poolMap{}
 	// filter by pool id - query this from Quicksilver (and cache hourly)
-	poolIds := poolMap{"osmosis-1": []int64{3, 625, 465}, "juno-1": []int64{2, 5, 7}}
+	for _, pool := range poolsManager.Get() {
+		for chain := range pool.Zones {
+			if _, ok := poolIds[chain]; !ok {
+				poolIds[chain] = make([]int64, 0)
+			}
+			poolIds[chain] = append(poolIds[chain], int64(pool.PoolID))
+		}
+	}
+
+	fmt.Println(poolIds)
 
 	msg := map[string]prewards.MsgSubmitClaim{}
 
@@ -106,18 +180,16 @@ OUTER:
 						msg[chainID] = prewards.MsgSubmitClaim{
 							UserAddress: address,
 							Zone:        chainID,
-							ProofType:   0,
-							Key:         make([][]byte, 0),
-							Data:        make([][]byte, 0),
-							ProofOps:    make([]*tmcrypto.ProofOps, 0),
-							Height:      abciquery.Response.Height,
+							SrcZone:     "quickosmo-1",
+							ClaimType:   prewards.ClaimTypeOsmosisPool,
+							Proofs:      make([]*prewards.Proof, 0),
 						}
 					}
 
 					abciquery, err := client.ABCIQueryWithOptions(
 						context.Background(), "/store/lockup/key",
 						append(osmolockup.KeyPrefixPeriodLock, append(osmolockup.KeyIndexSeparator, sdk.Uint64ToBigEndian(lockup.ID)...)...),
-						rpcclient.ABCIQueryOptions{Height: 0, Prove: true},
+						rpcclient.ABCIQueryOptions{Height: abciquery.Response.Height, Prove: true},
 					)
 					if err != nil {
 						return nil, err
@@ -129,9 +201,15 @@ OUTER:
 					}
 					chainMsg := msg[chainID]
 
-					chainMsg.Data = append(chainMsg.Data, abciquery.Response.Value)
-					chainMsg.ProofOps = append(chainMsg.ProofOps, abciquery.Response.ProofOps)
-					chainMsg.Key = append(chainMsg.Key, abciquery.Response.Key)
+					proof := prewards.Proof{
+						Data:     abciquery.Response.Value,
+						Key:      abciquery.Response.Key,
+						ProofOps: abciquery.Response.ProofOps,
+						Height:   abciquery.Response.Height,
+					}
+
+					chainMsg.Proofs = append(chainMsg.Proofs, &proof)
+
 					msg[chainID] = chainMsg
 					continue OUTER
 
@@ -145,13 +223,16 @@ OUTER:
 	for _, m := range msg {
 		out = append(out, m)
 	}
+
 	return out, nil
 
 }
 
 func main() {
+	connectionManager.Init("https://lcd.dev.quicksilver.zone/quicksilver/participationrewards/v1/protocoldata/connection", time.Minute*5)
+	poolsManager.Init("https://lcd.dev.quicksilver.zone/quicksilver/participationrewards/v1/protocoldata/osmosispools", time.Minute*5)
 	r := mux.NewRouter()
-	r.HandleFunc("/{address}/{timestamp}", handle)
+	r.HandleFunc("/{address}/{epoch}", handle)
 	http.Handle("/", r)
 	http.ListenAndServe(":8090", nil)
 }
--- xcclookup.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/ingenuity-build/xcclookup/cmd"
-
-func main() {
-	cmd.Execute()
-}
