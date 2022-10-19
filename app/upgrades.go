09740b16e1ffaef15b8b9ccbf964177b102d7254
Joe Bowman <joe@ingenuity.build>
2022-10-19 12:45:15 +0100

    call upgradeStoreLoader only within upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -1,14 +1,12 @@
 package app
 
 import (
-	"fmt"
-
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	claimsmanagertypes "github.com/ingenuity-build/quicksilver/x/claimsmanager/types"
+	cmtypes "github.com/ingenuity-build/quicksilver/x/claimsmanager/types"
 )
 
 // upgrade name consts: vMMmmppUpgradeName (M=Major, m=minor, p=patch)
@@ -16,41 +14,14 @@ const v001000UpgradeName = "v0.10.0"
 
 func setUpgradeHandlers(app *Quicksilver) {
 	app.UpgradeKeeper.SetUpgradeHandler(v001000UpgradeName, getv001000Upgrade(app))
-
-	// When a planned update height is reached, the old binary will panic
-	// writing on disk the height and name of the update that triggered it
-	// This will read that value, and execute the preparations for the upgrade.
-	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
-	if err != nil {
-		panic(fmt.Errorf("failed to read upgrade info from disk: %w", err))
-	}
-
-	if app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		return
-	}
-
-	var storeUpgrades *storetypes.StoreUpgrades
-
-	switch upgradeInfo.Name {
-	case v001000UpgradeName:
-
-		storeUpgrades = &storetypes.StoreUpgrades{
-			Added: []string{claimsmanagertypes.ModuleName},
-		}
-	default:
-		// no-op
-	}
-
-	if storeUpgrades != nil {
-		fmt.Println("UPGRADE: handler store loader upgrades")
-		// configure store loader that checks if version == upgradeHeight and applies store upgrades
-		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, storeUpgrades))
-	}
 }
 
 func getv001000Upgrade(app *Quicksilver) types.UpgradeHandler {
-	return func(ctx sdk.Context, _ types.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		app.UpgradeKeeper.Logger(ctx).Info("UPGRADE: to v0.10.0; no state transitions to apply.")
+	return func(ctx sdk.Context, plan types.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		app.UpgradeKeeper.Logger(ctx).Info("UPGRADE: to v0.10.0; upgrading store.")
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(plan.Height, &storetypes.StoreUpgrades{
+			Added: []string{cmtypes.ModuleName},
+		}))
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	}
 }
