51c69ad516f034ecf31a0cdb5b1e0108fa38fe1f
Elias Naur <elias@orijtech.com>
2022-10-19 13:41:08 -0600

    Add test for x/interchainstaking/keeper.AllBalancesCallback
    Basic test to improve coverage.
    
    Signed-off-by: Elias Naur <elias@orijtech.com>

--- x/interchainstaking/keeper/callbacks_test.go
+++ x/interchainstaking/keeper/callbacks_test.go
@@ -672,3 +672,29 @@ func (s *KeeperTestSuite) TestHandleValideRewardsCallback() {
 		s.Require().NoError(err)
 	})
 }
+
+func (s *KeeperTestSuite) TestAllBalancesCallback() {
+	s.Run("all balances", func() {
+		s.SetupTest()
+		s.SetupZones()
+
+		app := s.GetQuicksilverApp(s.chainA)
+		app.InterchainstakingKeeper.CallbackHandler().RegisterCallbacks()
+		ctx := s.chainA.GetContext()
+
+		zone, _ := app.InterchainstakingKeeper.GetZone(ctx, s.chainB.ChainID)
+
+		query := banktypes.QueryAllBalancesRequest{
+			Address: zone.DepositAddress.Address,
+		}
+		reqbz, err := app.AppCodec().Marshal(&query)
+		s.Require().NoError(err)
+
+		response := banktypes.QueryAllBalancesResponse{}
+		respbz, err := app.AppCodec().Marshal(&response)
+		s.Require().NoError(err)
+
+		err = keeper.AllBalancesCallback(app.InterchainstakingKeeper, ctx, respbz, icqtypes.Query{ChainId: s.chainB.ChainID, Request: reqbz})
+		s.Require().NoError(err)
+	})
+}
