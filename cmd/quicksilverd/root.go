6e30d1fbf7e422f7c49201870896a1afe89358cd
Joe Bowman <joe@ingenuity.build>
2022-10-20 11:59:16 +0100

    add osmosis forceprune

--- cmd/quicksilverd/root.go
+++ cmd/quicksilverd/root.go
@@ -95,6 +95,7 @@ func NewRootCmd() (*cobra.Command, app.EncodingConfig) {
 	}
 
 	rootCmd.AddCommand(
+		forceprune(),
 		genutilcli.InitCmd(app.ModuleBasics, app.DefaultNodeHome),
 		genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome),
 		genutilcli.GenTxCmd(app.ModuleBasics, encodingConfig.TxConfig, banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome),
