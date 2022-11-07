42f2fe70e750139ea8f1a20a565590204b40d6e3
Joe Bowman <joe@ingenuity.build>
2022-11-07 10:47:14 +0000

    temporary gc stale ibc updates

--- x/interchainquery/keeper/abci.go
+++ x/interchainquery/keeper/abci.go
@@ -21,6 +21,10 @@ func (k Keeper) EndBlocker(ctx sdk.Context) {
 	events := sdk.Events{}
 	// emit events for periodic queries
 	k.IterateQueries(ctx, func(_ int64, queryInfo types.Query) (stop bool) {
+		if queryInfo.QueryType == "ibc.ClientUpdate" && queryInfo.LastEmission.AddRaw(1000).GTE(sdk.NewInt(ctx.BlockHeight())) {
+			k.DeleteQuery(ctx, queryInfo.Id)
+			k.Logger(ctx).Error("Deleting stale query")
+		}
 		if queryInfo.LastEmission.IsNil() || queryInfo.LastEmission.IsZero() || queryInfo.LastEmission.Add(queryInfo.Period).Equal(sdk.NewInt(ctx.BlockHeight())) {
 			k.Logger(ctx).Info("Interchainquery event emitted", "id", queryInfo.Id)
 			event := sdk.NewEvent(
