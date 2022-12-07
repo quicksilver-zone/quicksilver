141e5ba68da7e46469abb993e1b264a781ac244d
Joe Bowman <joe@ingenuity.build>
2022-12-07 15:53:14 +0000

    remove mess in upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -210,14 +210,10 @@ func getv001004Upgrade(app *Quicksilver) upgradetypes.UpgradeHandler {
 					app.InterchainstakingKeeper.DeleteWithdrawalRecord(ctx, record.ChainId, record.Txhash, record.Status)
 					// unbonding completed, burn qAtoms to restore balance.
 				}
-				// this is hacky, but we know the surplus balance is 1100000 uatom.
-				if err := app.BankKeeper.BurnCoins(ctx, icstypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqatom", sdk.NewInt(1100000)))); err != nil {
-					panic(err)
-				}
 				return false
 			})
 
-			// this is hacky as shit, but we know the surplus balance is 1100000 uatom (2x 100000 suceeded, and then 1000000 failed with only 900000 in the account).
+			// this is hacky as shit, but we know the surplus balance is 1100000 uatom
 			if err := app.BankKeeper.BurnCoins(ctx, icstypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqatom", sdk.NewInt(1100000)))); err != nil {
 				panic(err)
 			}
