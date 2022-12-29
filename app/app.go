55221769990c34bfb780d852d81be12ba96d7a26
Joe Bowman <joe@ingenuity.build>
2022-12-29 14:50:45 +0000

    remove burn permissions from pr and mint module accounts

--- app/app.go
+++ app/app.go
@@ -206,10 +206,9 @@ var (
 
 	// module account permissions
 	maccPerms = map[string][]string{
-		authtypes.FeeCollectorName: nil,
-		distrtypes.ModuleName:      nil,
-		// TODO: Remove Burner from mint - for dev/test only;
-		minttypes.ModuleName:                       {authtypes.Minter, authtypes.Burner},
+		authtypes.FeeCollectorName:                 nil,
+		distrtypes.ModuleName:                      nil,
+		minttypes.ModuleName:                       {authtypes.Minter},
 		stakingtypes.BondedPoolName:                {authtypes.Burner, authtypes.Staking},
 		stakingtypes.NotBondedPoolName:             {authtypes.Burner, authtypes.Staking},
 		govtypes.ModuleName:                        {authtypes.Burner},
@@ -219,11 +218,10 @@ var (
 		interchainstakingtypes.ModuleName:          {authtypes.Minter},
 		interchainstakingtypes.EscrowModuleAccount: {authtypes.Burner},
 		interchainquerytypes.ModuleName:            nil,
-		// TODO: Remove Burner from participationrewards - for dev/test only;
-		participationrewardstypes.ModuleName: {authtypes.Burner},
-		airdroptypes.ModuleName:              nil,
-		wasm.ModuleName:                      {authtypes.Burner},
-		tokenfactorytypes.ModuleName:         {authtypes.Minter, authtypes.Burner},
+		participationrewardstypes.ModuleName:       nil,
+		airdroptypes.ModuleName:                    nil,
+		wasm.ModuleName:                            {authtypes.Burner},
+		tokenfactorytypes.ModuleName:               {authtypes.Minter, authtypes.Burner},
 	}
 
 	// module accounts that are allowed to receive tokens
