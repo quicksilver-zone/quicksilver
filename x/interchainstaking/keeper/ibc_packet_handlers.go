db4777b52f2fb7f4c7b6b4d43a31c45d7715edbb
Joe Bowman <joe@ingenuity.build>
2022-12-30 10:14:15 +0000

    return nil not error on ack_error receipt, else we just get the same acknowledgement repeatedly submitted indefinitely

--- x/interchainstaking/keeper/ibc_packet_handlers.go
+++ x/interchainstaking/keeper/ibc_packet_handlers.go
@@ -51,7 +51,8 @@ func (k *Keeper) HandleAcknowledgement(ctx sdk.Context, packet channeltypes.Pack
 
 		k.Logger(ctx).Error("received an acknowledgement error", "error", err, "remote_err", ackErr, "data", acknowledgement)
 		defer telemetry.IncrCounter(1, types.ModuleName, "ica_acknowledgement_errors")
-		return errors.New("received an acknowledgement error; unable to process")
+		return nil
+		//return errors.New("received an acknowledgement error; unable to process") // this just causes the same errAck to be submitted repeatedly.
 	}
 	defer telemetry.IncrCounter(1, types.ModuleName, "ica_acknowledgement_success")
 	txMsgData := &sdk.TxMsgData{}
@@ -108,7 +109,6 @@ func (k *Keeper) HandleAcknowledgement(ctx sdk.Context, packet channeltypes.Pack
 				return err
 			}
 			k.Logger(ctx).Info("Shares tokenized", "response", response)
-			// check tokenizedShareTransfers (inc. rebalance and unbond)
 			if err := k.HandleTokenizedShares(ctx, src, response.Amount, packetData.Memo); err != nil {
 				return err
 			}
