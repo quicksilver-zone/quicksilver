68a45236bc4630b05c3346971eb7d63b3f876ac5
Ajaz Ahmed Ansari <ajansari9585@gmail.com>
2023-02-07 00:04:28 +0530

    check for innuendo specific upgrade-handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -84,95 +84,96 @@ func v010400UpgradeHandler(app *Quicksilver) upgradetypes.UpgradeHandler {
 			r.Completed = &time
 			app.InterchainstakingKeeper.SetReceipt(ctx, r)
 		}
+		if ctx.ChainID() == "innuendo-5" {
 
-		// clear uni-5 unbondings
-		app.InterchainstakingKeeper.IteratePrefixedUnbondingRecords(ctx, []byte("uni-5"), func(_ int64, record types.UnbondingRecord) (stop bool) {
-			app.InterchainstakingKeeper.DeleteUnbondingRecord(ctx, record.ChainId, record.Validator, record.EpochNumber)
-			return false
-		})
+			// clear uni-5 unbondings
+			app.InterchainstakingKeeper.IteratePrefixedUnbondingRecords(ctx, []byte("uni-5"), func(_ int64, record types.UnbondingRecord) (stop bool) {
+				app.InterchainstakingKeeper.DeleteUnbondingRecord(ctx, record.ChainId, record.Validator, record.EpochNumber)
+				return false
+			})
 
-		// clear uni-5 redelegations
-		app.InterchainstakingKeeper.IteratePrefixedRedelegationRecords(ctx, []byte("uni-5"), func(_ int64, _ []byte, record types.RedelegationRecord) (stop bool) {
-			app.InterchainstakingKeeper.DeleteRedelegationRecord(ctx, record.ChainId, record.Source, record.Destination, record.EpochNumber)
-			return false
-		})
+			// clear uni-5 redelegations
+			app.InterchainstakingKeeper.IteratePrefixedRedelegationRecords(ctx, []byte("uni-5"), func(_ int64, _ []byte, record types.RedelegationRecord) (stop bool) {
+				app.InterchainstakingKeeper.DeleteRedelegationRecord(ctx, record.ChainId, record.Source, record.Destination, record.EpochNumber)
+				return false
+			})
 
-		// remove uni-5 zone and related records
-		app.InterchainstakingKeeper.IterateZones(ctx, func(index int64, zone types.Zone) (stop bool) {
-			if zone.ChainId == "uni-5" {
-				// remove uni-5 delegation records
-				app.InterchainstakingKeeper.IterateAllDelegations(ctx, &zone, func(delegation types.Delegation) (stop bool) {
-					err := app.InterchainstakingKeeper.RemoveDelegation(ctx, &zone, delegation)
-					if err != nil {
-						panic(err)
-					}
-					return false
-				})
+			// remove uni-5 zone and related records
+			app.InterchainstakingKeeper.IterateZones(ctx, func(index int64, zone types.Zone) (stop bool) {
+				if zone.ChainId == "uni-5" {
+					// remove uni-5 delegation records
+					app.InterchainstakingKeeper.IterateAllDelegations(ctx, &zone, func(delegation types.Delegation) (stop bool) {
+						err := app.InterchainstakingKeeper.RemoveDelegation(ctx, &zone, delegation)
+						if err != nil {
+							panic(err)
+						}
+						return false
+					})
 
-				// remove uni-5 performance delegation records
-				app.InterchainstakingKeeper.IterateAllPerformanceDelegations(ctx, &zone, func(delegation types.Delegation) (stop bool) {
-					err := app.InterchainstakingKeeper.RemoveDelegation(ctx, &zone, delegation)
-					if err != nil {
-						panic(err)
-					}
-					return false
-				})
-				// remove uni-5 receipts
-				app.InterchainstakingKeeper.IterateZoneReceipts(ctx, &zone, func(index int64, receiptInfo types.Receipt) (stop bool) {
-					app.InterchainstakingKeeper.DeleteReceipt(ctx, icskeeper.GetReceiptKey(receiptInfo.ChainId, receiptInfo.Txhash))
-					return false
-				})
+					// remove uni-5 performance delegation records
+					app.InterchainstakingKeeper.IterateAllPerformanceDelegations(ctx, &zone, func(delegation types.Delegation) (stop bool) {
+						err := app.InterchainstakingKeeper.RemoveDelegation(ctx, &zone, delegation)
+						if err != nil {
+							panic(err)
+						}
+						return false
+					})
+					// remove uni-5 receipts
+					app.InterchainstakingKeeper.IterateZoneReceipts(ctx, &zone, func(index int64, receiptInfo types.Receipt) (stop bool) {
+						app.InterchainstakingKeeper.DeleteReceipt(ctx, icskeeper.GetReceiptKey(receiptInfo.ChainId, receiptInfo.Txhash))
+						return false
+					})
 
-				// remove zone withdrawal records
-				app.InterchainstakingKeeper.IterateZoneWithdrawalRecords(ctx, zone.ChainId, func(index int64, record types.WithdrawalRecord) (stop bool) {
-					app.InterchainstakingKeeper.DeleteWithdrawalRecord(ctx, zone.ChainId, record.Txhash, record.Status)
-					return false
-				})
+					// remove zone withdrawal records
+					app.InterchainstakingKeeper.IterateZoneWithdrawalRecords(ctx, zone.ChainId, func(index int64, record types.WithdrawalRecord) (stop bool) {
+						app.InterchainstakingKeeper.DeleteWithdrawalRecord(ctx, zone.ChainId, record.Txhash, record.Status)
+						return false
+					})
 
-				app.InterchainstakingKeeper.DeleteZone(ctx, zone.ChainId)
+					app.InterchainstakingKeeper.DeleteZone(ctx, zone.ChainId)
 
+				}
+				return false
+			})
+
+			// remove uni-5 queries in state
+			app.InterchainQueryKeeper.IterateQueries(ctx, func(_ int64, queryInfo icqtypes.Query) (stop bool) {
+				if queryInfo.ChainId == "uni-5" {
+					app.InterchainQueryKeeper.DeleteQuery(ctx, queryInfo.Id)
+				}
+				return false
+			})
+
+			// burn uqjunox
+			addr1, err := AccAddressFromBech32("quick17v9kk34km3w6hdjs2sn5s5qjdu2zrm0m3rgtmq", "quick")
+			if err != nil {
+				return nil, err
 			}
-			return false
-		})
-
-		// remove uni-5 queries in state
-		app.InterchainQueryKeeper.IterateQueries(ctx, func(_ int64, queryInfo icqtypes.Query) (stop bool) {
-			if queryInfo.ChainId == "uni-5" {
-				app.InterchainQueryKeeper.DeleteQuery(ctx, queryInfo.Id)
+			addr2, err := AccAddressFromBech32("quick16x03wcp37kx5e8ehckjxvwcgk9j0cqnhcccnty", "quick")
+			if err != nil {
+				return nil, err
 			}
-			return false
-		})
 
-		// burn uqjunox
-		addr1, err := AccAddressFromBech32("quick17v9kk34km3w6hdjs2sn5s5qjdu2zrm0m3rgtmq", "quick")
-		if err != nil {
-			return nil, err
-		}
-		addr2, err := AccAddressFromBech32("quick16x03wcp37kx5e8ehckjxvwcgk9j0cqnhcccnty", "quick")
-		if err != nil {
-			return nil, err
-		}
+			err = app.BankKeeper.SendCoinsFromAccountToModule(ctx, addr1, tokenfactorytypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqjunox", sdkmath.NewInt(1600000))))
+			if err != nil {
+				return nil, err
+			}
 
-		err = app.BankKeeper.SendCoinsFromAccountToModule(ctx, addr1, tokenfactorytypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqjunox", sdkmath.NewInt(1600000))))
-		if err != nil {
-			return nil, err
-		}
+			err = app.BankKeeper.SendCoinsFromAccountToModule(ctx, addr2, tokenfactorytypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqjunox", sdkmath.NewInt(200000000))))
+			if err != nil {
+				return nil, err
+			}
 
-		err = app.BankKeeper.SendCoinsFromAccountToModule(ctx, addr2, tokenfactorytypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqjunox", sdkmath.NewInt(200000000))))
-		if err != nil {
-			return nil, err
-		}
+			err = app.BankKeeper.SendCoinsFromModuleToModule(ctx, types.EscrowModuleAccount, tokenfactorytypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqjunox", sdkmath.NewInt(400000))))
+			if err != nil {
+				return nil, err
+			}
 
-		err = app.BankKeeper.SendCoinsFromModuleToModule(ctx, types.EscrowModuleAccount, tokenfactorytypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqjunox", sdkmath.NewInt(400000))))
-		if err != nil {
-			return nil, err
+			err = app.BankKeeper.BurnCoins(ctx, tokenfactorytypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqjunox", sdkmath.NewInt(202000000))))
+			if err != nil {
+				return nil, err
+			}
 		}
-
-		err = app.BankKeeper.BurnCoins(ctx, tokenfactorytypes.ModuleName, sdk.NewCoins(sdk.NewCoin("uqjunox", sdkmath.NewInt(202000000))))
-		if err != nil {
-			return nil, err
-		}
-
 		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 	}
 }
