45ca2dada1d7aabbf97252361fb4d77eadd1e8b4
aljo242 <alex@shmeeload.xyz>
2023-02-22 14:03:28 -0500

    randomize

--- x/tokenfactory/simulation/genesis.go
+++ x/tokenfactory/simulation/genesis.go
@@ -12,11 +12,13 @@ import (
 	"github.com/ingenuity-build/quicksilver/x/tokenfactory/types"
 )
 
-// RandomizedGenState generates a random GenesisState for mint
+// RandomizedGenState generates a random GenesisState for tokenfactory.
 func RandomizedGenState(simState *module.SimulationState) {
+	feeAmt := simState.Rand.Int63n(5_000_000) + 5_000_000 // [5_000_000, 10_000_000)
+
 	tokenfactoryGenesis := types.DefaultGenesis()
 	// use bond denom for simulation
-	tokenfactoryGenesis.Params.DenomCreationFee = sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10_000_000))
+	tokenfactoryGenesis.Params.DenomCreationFee = sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, feeAmt))
 	err := tokenfactoryGenesis.Validate()
 	if err != nil {
 		panic(err)
