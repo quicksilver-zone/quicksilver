3c202e9e9760eaad784997f1c4dfeb96eeb1cafb
aljo242 <alex@shmeeload.xyz>
2023-03-08 11:49:16 -0500

    import genesis

--- x/interchainstaking/simulation/genesis.go
+++ x/interchainstaking/simulation/genesis.go
@@ -3,57 +3,37 @@ package simulation
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
 	"github.com/ingenuity-build/quicksilver/x/interchainstaking/types"
 )
 
+// DONTCOVER
+
 // RandomizedGenState generates a random GenesisState for interchainstaking
 func RandomizedGenState(simState *module.SimulationState) {
-	zones := []types.Zone{
-		{
-			ConnectionId:     "connection-77001",
-			ChainId:          "cosmoshub-4",
-			AccountPrefix:    "cosmos",
-			LocalDenom:       "uqatom",
-			BaseDenom:        "uatom",
-			MultiSend:        false,
-			LiquidityModule:  false,
-			UnbondingEnabled: true,
-		},
-		{
-			ConnectionId:     "connection-77002",
-			ChainId:          "osmosis-1",
-			AccountPrefix:    "osmo",
-			LocalDenom:       "uqosmo",
-			BaseDenom:        "uosmo",
-			MultiSend:        false,
-			LiquidityModule:  false,
-			UnbondingEnabled: true,
-		},
-		{
-			ConnectionId:     "connection-77003",
-			ChainId:          "uni-5",
-			AccountPrefix:    "juno",
-			LocalDenom:       "uqjunox",
-			BaseDenom:        "ujunox",
-			MultiSend:        false,
-			LiquidityModule:  false,
-			UnbondingEnabled: true,
-		},
+	bz, err := os.ReadFile("./testdata/zones.json")
+	if err != nil {
+		panic(err)
 	}
 
-	for _, z := range zones {
-		// set zone validators
-		z.Validators = append(z.Validators, &types.Validator{ValoperAddress: z.AccountPrefix + "valoper1sjllsnramtg3ewxqwwrwjxfgc4n4ef9u2lcnj0", CommissionRate: sdk.MustNewDecFromStr("0.2"), VotingPower: sdk.NewInt(2000), Status: stakingtypes.BondStatusBonded})
-		z.Validators = append(z.Validators, &types.Validator{ValoperAddress: z.AccountPrefix + "valoper156gqf9837u7d4c4678yt3rl4ls9c5vuursrrzf", CommissionRate: sdk.MustNewDecFromStr("0.2"), VotingPower: sdk.NewInt(2000), Status: stakingtypes.BondStatusBonded})
-		z.Validators = append(z.Validators, &types.Validator{ValoperAddress: z.AccountPrefix + "valoper14lultfckehtszvzw4ehu0apvsr77afvyju5zzy", CommissionRate: sdk.MustNewDecFromStr("0.2"), VotingPower: sdk.NewInt(2000), Status: stakingtypes.BondStatusBonded})
-		z.Validators = append(z.Validators, &types.Validator{ValoperAddress: z.AccountPrefix + "valoper1a3yjj7d3qnx4spgvjcwjq9cw9snrrrhu5h6jll", CommissionRate: sdk.MustNewDecFromStr("0.2"), VotingPower: sdk.NewInt(2000), Status: stakingtypes.BondStatusBonded})
-		z.Validators = append(z.Validators, &types.Validator{ValoperAddress: z.AccountPrefix + "valoper1z8zjv3lntpwxua0rtpvgrcwl0nm0tltgpgs6l7", CommissionRate: sdk.MustNewDecFromStr("0.2"), VotingPower: sdk.NewInt(2000), Status: stakingtypes.BondStatusBonded})
-		z.Validators = append(z.Validators, &types.Validator{ValoperAddress: z.AccountPrefix + "valoper1qaa9zej9a0ge3ugpx3pxyx602lxh3ztqgfnp42", CommissionRate: sdk.MustNewDecFromStr("0.2"), VotingPower: sdk.NewInt(2000), Status: stakingtypes.BondStatusBonded})
+	var zones []types.Zone
+	err = json.Unmarshal(bz, &zones)
+	if err != nil {
+		panic(err)
+	}
+
+	bz, err = os.ReadFile("./testdata/delegations.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var delegations []types.DelegationsForZone
+	err = json.Unmarshal(bz, &delegations)
+	if err != nil {
+		panic(err)
 	}
 
 	params := types.DefaultParams()
@@ -63,14 +43,14 @@ func RandomizedGenState(simState *module.SimulationState) {
 		Params:                 params,
 		Zones:                  zones,
 		Receipts:               nil,
-		Delegations:            nil,
+		Delegations:            delegations,
 		PerformanceDelegations: nil,
 		DelegatorIntents:       nil,
 		PortConnections:        nil,
 		WithdrawalRecords:      nil,
 	}
 
-	bz, err := json.MarshalIndent(&icsGenesis, "", " ")
+	bz, err = json.MarshalIndent(&icsGenesis, "", " ")
 	if err != nil {
 		panic(err)
 	}
