0d6b69cb2c8ff0b686559bd0de5e023a2589e2bb
Joe Bowman <joe@ingenuity.build>
2023-03-15 20:22:19 +0000

    make epoch identifier a constant

--- x/participationrewards/keeper/hooks.go
+++ x/participationrewards/keeper/hooks.go
@@ -15,7 +15,7 @@ func (k Keeper) BeforeEpochStart(_ sdk.Context, _ string, _ int64) error {
 }
 
 func (k Keeper) AfterEpochEnd(ctx sdk.Context, epochIdentifier string, epochNumber int64) error {
-	if epochIdentifier == "epoch" {
+	if epochIdentifier == epochstypes.EpochIdentifierEpoch {
 		k.IteratePrefixedProtocolDatas(ctx, types.GetPrefixProtocolDataKey(types.ProtocolDataTypeConnection), func(index int64, data types.ProtocolData) (stop bool) {
 			blockQuery := tmservice.GetLatestBlockRequest{}
 			bz := k.cdc.MustMarshal(&blockQuery)
