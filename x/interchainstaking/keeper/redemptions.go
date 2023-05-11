d58a0b5e38c167c52f88e39c1f65aec9d3ac3950
Joe Bowman <joe@ingenuity.build>
2023-05-11 15:14:02 +0100

    debug: Additional logging in panic statement for GetUnlockedTokensForZone [QCK-425]

--- x/interchainstaking/keeper/redemptions.go
+++ x/interchainstaking/keeper/redemptions.go
@@ -113,7 +113,7 @@ func (k *Keeper) GetUnlockedTokensForZone(ctx sdk.Context, zone *types.Zone) (ma
 		if found {
 			availablePerValidator[redelegation.Destination] = thisAvailable.Sub(sdk.NewInt(redelegation.Amount))
 			if availablePerValidator[redelegation.Destination].LT(sdk.ZeroInt()) {
-				panic("negative available amount; unable to continue")
+				panic(fmt.Sprintf("negative available amount [chain: %s, validator: %s, amount: %s]; unable to continue", zone.ChainId, redelegation.Destination, availablePerValidator[redelegation.Destination].String()))
 			}
 			total = total.Sub(sdk.NewInt(redelegation.Amount))
 		}
