212776771f92cb3b085c48684a5e733748b06f59
muku314115 <mukangotan@gmail.com>
2023-06-08 12:11:56 +0530

    minor refactor problem

--- x/interchainstaking/keeper/hooks.go
+++ x/interchainstaking/keeper/hooks.go
@@ -122,7 +122,7 @@ func (k *Keeper) AfterEpochEnd(ctx sdk.Context, epochIdentifier string, epochNum
 				zone.WithdrawalWaitgroup = 0
 			}
 
-			// OnChanOpenAck calls SetWithdrawalAddress (see ibc_middleware.go)
+			// OnChanOpenAck calls SetWithdrawalAddress (see ibc_module.go)
 			k.Logger(ctx).Info(
 				"withdrawing rewards",
 				"chain_id", zone.ChainId,
