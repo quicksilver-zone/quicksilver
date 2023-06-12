47c1850933b9eebcc4085b5989241d5043339f13
muku314115 <mukangotan@gmail.com>
2023-06-12 17:33:52 +0530

    minor changes for uniformity

--- x/participationrewards/keeper/rewards_lockup_test.go
+++ x/participationrewards/keeper/rewards_lockup_test.go
@@ -10,40 +10,40 @@ func (s *KeeperTestSuite) TestAllocateLockupRewards() {
 
 	tests := []struct {
 		name          string
-		getAllocation func(*app.Quicksilver, types.Context) int64
+		getAllocation func(types.Context, *app.Quicksilver) int64
 		wantErr       bool
 	}{
 		{
 			"valid unit allocation",
-			func(appA *app.Quicksilver, ctx types.Context) int64 {
+			func(ctx types.Context, appA *app.Quicksilver) int64 {
 				return 1
 			},
 			false,
 		},
 		{
 			"invalid -ve allocation",
-			func(appA *app.Quicksilver, ctx types.Context) int64 {
+			func(ctx types.Context, appA *app.Quicksilver) int64 {
 				return -1
 			},
 			true,
 		},
 		{
 			"redundant allocation",
-			func(appA *app.Quicksilver, ctx types.Context) int64 {
+			func(ctx types.Context, appA *app.Quicksilver) int64 {
 				return 0
 			},
 			false,
 		},
 		{
 			"valid total module balance allocation",
-			func(appA *app.Quicksilver, ctx types.Context) int64 {
+			func(ctx types.Context, appA *app.Quicksilver) int64 {
 				return appA.ParticipationRewardsKeeper.GetModuleBalance(ctx).Int64()
 			},
 			false,
 		},
 		{
 			"invalid greater than module balance allocation",
-			func(appA *app.Quicksilver, ctx types.Context) int64 {
+			func(ctx types.Context, appA *app.Quicksilver) int64 {
 				return appA.ParticipationRewardsKeeper.GetModuleBalance(ctx).Int64() + 1
 			},
 			true,
@@ -64,7 +64,7 @@ func (s *KeeperTestSuite) TestAllocateLockupRewards() {
 			appA.ParticipationRewardsKeeper.SetParams(ctx, params)
 
 			initialBalance := appA.ParticipationRewardsKeeper.GetModuleBalance(ctx).Int64()
-			allocation := tt.getAllocation(appA, ctx)
+			allocation := tt.getAllocation(ctx, appA)
 
 			if err := appA.ParticipationRewardsKeeper.AllocateLockupRewards(ctx, math.NewInt(allocation)); err != nil {
 				s.Require().True((err != nil) == tt.wantErr)
