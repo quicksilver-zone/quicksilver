6afeb13cab442ba65627768fb551c6c55b556ced
muku314115 <mukangotan@gmail.com>
2023-06-14 12:32:41 +0530

    distribution scores tests

--- x/participationrewards/keeper/rewards_validatorSelection_test.go
+++ x/participationrewards/keeper/rewards_validatorSelection_test.go
@@ -6,11 +6,15 @@ import (
 	"github.com/ingenuity-build/quicksilver/utils/addressutils"
 	icstypes "github.com/ingenuity-build/quicksilver/x/interchainstaking/types"
 	"github.com/ingenuity-build/quicksilver/x/participationrewards/types"
+	"strings"
+)
+
+var (
+	user1, _ = addressutils.AccAddressFromBech32("cosmos1kcgprgjxntc5w4ygfsgvjnnypeptf3vw6gyv0z77h27cx23vg5rsptlw4a", "")
+	user2, _ = addressutils.AccAddressFromBech32("cosmos1u4ln57y5m2qyna7aq09u3r05waf74ad9rsk4hzr79acapar6lhqqumtd5d", "")
 )
 
 func (s *KeeperTestSuite) TestCalcUserValidatorSelectionAllocations() {
-	user1, _ := addressutils.AccAddressFromBech32("cosmos1kcgprgjxntc5w4ygfsgvjnnypeptf3vw6gyv0z77h27cx23vg5rsptlw4a", "")
-	user2, _ := addressutils.AccAddressFromBech32("cosmos1u4ln57y5m2qyna7aq09u3r05waf74ad9rsk4hzr79acapar6lhqqumtd5d", "")
 
 	tests := []struct {
 		name            string
@@ -230,6 +234,151 @@ func (s *KeeperTestSuite) TestCalcUserValidatorSelectionAllocations() {
 
 			userAllocations := appA.ParticipationRewardsKeeper.CalcUserValidatorSelectionAllocations(ctx, &zone, zs)
 			s.Require().Equal(tt.want, userAllocations)
+		})
+	}
+}
+func (s *KeeperTestSuite) TestCalcDistributionScores() {
+
+	tests := []struct {
+		name            string
+		malleate        func(sdk.Context, *app.Quicksilver)
+		validatorScores func(sdk.Context, *app.Quicksilver, string) map[string]*types.Validator
+		verify          func(zs types.ZoneScore)
+		wantErr         bool
+	}{
+		{
+			name: "zero validators",
+			malleate: func(ctx sdk.Context, appA *app.Quicksilver) {
+				zone, _ := appA.InterchainstakingKeeper.GetZone(ctx, s.chainB.ChainID)
+				appA.InterchainstakingKeeper.IterateDelegatorIntents(ctx, &zone, true, func(_ int64, di icstypes.DelegatorIntent) (stop bool) {
+					appA.InterchainstakingKeeper.DeleteDelegatorIntent(ctx, &zone, di.Delegator, true)
+					return false
+				})
+				zone.ValidatorSelectionAllocation = 5000
+
+				for _, val := range appA.InterchainstakingKeeper.GetValidators(ctx, zone.ChainId) {
+					address, _ := val.GetAddressBytes()
+					appA.InterchainstakingKeeper.DeleteValidator(ctx, zone.ChainId, address)
+				}
+
+				appA.InterchainstakingKeeper.SetZone(ctx, &zone)
+			},
+			validatorScores: func(ctx sdk.Context, appA *app.Quicksilver, chainId string) map[string]*types.Validator {
+				return nil
+			},
+			verify: func(zs types.ZoneScore) {
+			},
+			wantErr: true,
+		},
+		{
+			name: "zero voting power",
+			malleate: func(ctx sdk.Context, appA *app.Quicksilver) {
+				zone, _ := appA.InterchainstakingKeeper.GetZone(ctx, s.chainB.ChainID)
+				appA.InterchainstakingKeeper.IterateDelegatorIntents(ctx, &zone, true, func(_ int64, di icstypes.DelegatorIntent) (stop bool) {
+					appA.InterchainstakingKeeper.DeleteDelegatorIntent(ctx, &zone, di.Delegator, true)
+					return false
+				})
+				zone.ValidatorSelectionAllocation = 5000
+
+				for _, val := range appA.InterchainstakingKeeper.GetValidators(ctx, zone.ChainId) {
+					val.VotingPower = sdk.NewInt(0)
+					appA.InterchainstakingKeeper.SetValidator(ctx, zone.ChainId, val)
+				}
+
+				appA.InterchainstakingKeeper.SetZone(ctx, &zone)
+			},
+			validatorScores: func(ctx sdk.Context, appA *app.Quicksilver, chainId string) map[string]*types.Validator {
+				validatorScores := make(map[string]*types.Validator)
+
+				for _, val := range appA.InterchainstakingKeeper.GetValidators(ctx, chainId) {
+					val.Score = sdk.NewDec(1)
+					validatorScores[val.ValoperAddress] = &types.Validator{
+						PowerPercentage:   sdk.NewDec(1),
+						DistributionScore: sdk.NewDec(1),
+						PerformanceScore:  sdk.NewDec(1),
+						Validator:         &val,
+					}
+				}
+				return validatorScores
+			},
+			verify: func(zs types.ZoneScore) {
+			},
+			wantErr: true,
+		},
+		{
+			name: "valid zonescore, same power",
+			malleate: func(ctx sdk.Context, appA *app.Quicksilver) {
+				zone, _ := appA.InterchainstakingKeeper.GetZone(ctx, s.chainB.ChainID)
+				appA.InterchainstakingKeeper.IterateDelegatorIntents(ctx, &zone, true, func(_ int64, di icstypes.DelegatorIntent) (stop bool) {
+					appA.InterchainstakingKeeper.DeleteDelegatorIntent(ctx, &zone, di.Delegator, true)
+					return false
+				})
+
+				for _, val := range appA.InterchainstakingKeeper.GetValidators(ctx, zone.ChainId) {
+					val.VotingPower = sdk.NewInt(10)
+					appA.InterchainstakingKeeper.SetValidator(ctx, zone.ChainId, val)
+				}
+				zone.ValidatorSelectionAllocation = 5000
+
+				appA.InterchainstakingKeeper.SetZone(ctx, &zone)
+			},
+			validatorScores: func(ctx sdk.Context, appA *app.Quicksilver, chainId string) map[string]*types.Validator {
+				validatorScores := make(map[string]*types.Validator)
+
+				for _, val := range appA.InterchainstakingKeeper.GetValidators(ctx, chainId) {
+					validatorScores[val.ValoperAddress] = &types.Validator{
+						PowerPercentage:   sdk.NewDec(1),
+						DistributionScore: sdk.NewDec(1),
+						PerformanceScore:  sdk.NewDec(1),
+						Validator:         &val,
+					}
+				}
+				return validatorScores
+			},
+			verify: func(zs types.ZoneScore) {
+				s.Require().Equal(zs.TotalVotingPower, sdk.NewInt(40))
+
+				for _, val := range zs.ValidatorScores {
+					s.Require().Equal(strings.TrimRight(val.PowerPercentage.String(), "0"), "0.25")
+					s.Require().Equal(val.DistributionScore, sdk.NewDec(1))
+				}
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		s.Run(tt.name, func() {
+			s.SetupTest()
+
+			appA := s.GetQuicksilverApp(s.chainA)
+			ctx := s.chainA.GetContext()
+
+			params := appA.ParticipationRewardsKeeper.GetParams(ctx)
+			params.ClaimsEnabled = true
+			appA.ParticipationRewardsKeeper.SetParams(ctx, params)
+
+			tt.malleate(ctx, appA)
+
+			zone, found := appA.InterchainstakingKeeper.GetZone(ctx, s.chainB.ChainID)
+			s.Require().True(found)
+
+			s.Require().NoError(appA.BankKeeper.MintCoins(ctx, "mint", sdk.NewCoins(sdk.NewCoin(appA.StakingKeeper.BondDenom(ctx), sdk.NewIntFromUint64(zone.HoldingsAllocation)))))
+			s.Require().NoError(appA.BankKeeper.SendCoinsFromModuleToModule(ctx, "mint", types.ModuleName, sdk.NewCoins(sdk.NewCoin(appA.StakingKeeper.BondDenom(ctx), sdk.NewIntFromUint64(zone.HoldingsAllocation)))))
+
+			validatorScores := tt.validatorScores(ctx, appA, zone.ChainId)
+
+			zs := types.ZoneScore{
+				ZoneID:           zone.ChainId,
+				TotalVotingPower: sdk.NewInt(0),
+				ValidatorScores:  validatorScores,
+			}
+
+			err := appA.ParticipationRewardsKeeper.CalcDistributionScores(ctx, zone, &zs)
+			s.Require().Equal(err != nil, tt.wantErr)
+			tt.verify(zs)
 		})
 	}
 }
