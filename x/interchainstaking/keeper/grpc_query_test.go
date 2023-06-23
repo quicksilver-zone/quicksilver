27ac85c336ed2ae73647633b295b65097956a9b7
Muku <44918265+muku314115@users.noreply.github.com>
2023-06-23 17:13:02 +0530

    test: end point to get intents across all zones for a delegator address #466 (#468)
    * front-port v1.2.13
    
    * fix delegation flush test
    
    * add tests for HandleDepositTransaction
    
    * update tests for new error handling
    
    * remove superflous field in proto
    
    * final v1.2.13 front-port and test fixes
    
    * s/s/suite/
    
    * remove fmt.Printlns used for debugging
    
    * update participation rewards and protocoldatas, to add validation, automatic key generation, basechain/basedenom in osmosisparams and updated tvl calculation
    
    * front-port v1.2.13
    
    * remove balancerPoolPretty json marshal/unmarshal, as we deal with raw KV store objects; switch to native bech32 decode/encode as we dont expect prefix of pools to match sdk.GetConfig().Bech32Prefix
    
    * fix: allow support for multiple denoms from same zone in tvl calc
    
    * lint
    
    * fix: refactor utils/address.go to avoid import cycle when using in osmosis-types
    
    * add test for randomutils
    
    * fix: remove potential non-det by iterating over maps using sorted keys
    
    * resolve outstanding nits
    
    * lint
    
    * fix nits
    
    * bump go to 1.20.5
    
    * Update x/interchainstaking/keeper/receipt.go
    
    * Apply suggestions from code review
    
    * lint
    
    * bump hermes to v1.5.0
    
    * update icq and relayer in test scripts
    
    * copying
    
    * Update x/interchainstaking/keeper/grpc_query.go
    
    Co-authored-by: Alex Johnson <alex@shmeeload.xyz>
    
    * adding nil check
    
    * proto format
    
    ---------
    
    Co-authored-by: Joe Bowman <joe@ingenuity.build>
    Co-authored-by: Alex Johnson <alex@shmeeload.xyz>

--- x/interchainstaking/keeper/grpc_query_test.go
+++ x/interchainstaking/keeper/grpc_query_test.go
@@ -13,6 +13,8 @@ import (
 	"github.com/ingenuity-build/quicksilver/x/interchainstaking/types"
 )
 
+var delegatorAddress = "quick16pxh2v4hr28h2gkntgfk8qgh47pfmjfhzgeure"
+
 func (suite *KeeperTestSuite) TestKeeper_Zones() {
 	icsKeeper := suite.GetQuicksilverApp(suite.chainA).InterchainstakingKeeper
 	ctx := suite.chainA.GetContext()
@@ -313,6 +315,136 @@ func (suite *KeeperTestSuite) TestKeeper_DelegatorIntent() {
 	}
 }
 
+func (suite *KeeperTestSuite) TestKeeper_DelegatorIntents() {
+	icsKeeper := suite.GetQuicksilverApp(suite.chainA).InterchainstakingKeeper
+	ctx := suite.chainA.GetContext()
+
+	tests := []struct {
+		name     string
+		malleate func()
+		req      *types.QueryDelegatorIntentsRequest
+		wantErr  bool
+		verify   func(delegation []*types.DelegatorIntentsResponse)
+	}{
+		{
+			name:     "DelegatorIntent_Nil_Request",
+			malleate: func() {},
+			req:      nil,
+			wantErr:  true,
+			verify: func([]*types.DelegatorIntentsResponse) {
+			},
+		},
+		{
+			"DelegatorIntent_No_Delegator_Intents",
+			func() {
+			},
+			&types.QueryDelegatorIntentsRequest{
+				DelegatorAddress: testAddress,
+			},
+			false,
+			func(intents []*types.DelegatorIntentsResponse) {
+				for _, intent := range intents {
+					suite.Require().Equal(intent.ChainId, suite.chainB.ChainID)
+					suite.Require().Equal(len(intent.Intent.Intents), 0)
+				}
+			},
+		},
+		{
+			"DelegatorIntent_Valid_Intents across multiple zones",
+			func() {
+				zone, found := icsKeeper.GetZone(ctx, suite.chainB.ChainID)
+				suite.Require().True(found)
+				// give funds
+				suite.giveFunds(ctx, zone.LocalDenom, 5000000, testAddress)
+				// set intents
+				intents := []types.DelegatorIntent{
+					{
+						Delegator: testAddress,
+						Intents: types.ValidatorIntents{
+							&types.ValidatorIntent{
+								ValoperAddress: icsKeeper.GetValidators(ctx, suite.chainB.ChainID)[0].ValoperAddress,
+								Weight:         sdk.OneDec(),
+							},
+						},
+					},
+				}
+				for _, intent := range intents {
+					icsKeeper.SetDelegatorIntent(ctx, &zone, intent, false)
+				}
+
+				// cosmos zone
+				zone = types.Zone{
+					ConnectionId:    "connection-77001",
+					ChainId:         "cosmoshub-4",
+					AccountPrefix:   "cosmos",
+					LocalDenom:      "uqatom",
+					BaseDenom:       "uatom",
+					MultiSend:       false,
+					LiquidityModule: false,
+					Is_118:          true,
+				}
+				(&icsKeeper).SetZone(ctx, &zone)
+				// give funds
+				suite.giveFunds(ctx, zone.LocalDenom, 5000000, testAddress)
+				// set intents
+				intents = []types.DelegatorIntent{
+					{
+						Delegator: testAddress,
+						Intents: types.ValidatorIntents{
+							&types.ValidatorIntent{
+								ValoperAddress: icsKeeper.GetValidators(ctx, suite.chainB.ChainID)[0].ValoperAddress,
+								Weight:         sdk.OneDec(),
+							},
+						},
+					},
+				}
+				for _, intent := range intents {
+					icsKeeper.SetDelegatorIntent(ctx, &zone, intent, false)
+				}
+			},
+			&types.QueryDelegatorIntentsRequest{
+				DelegatorAddress: testAddress,
+			},
+			false,
+			func(intents []*types.DelegatorIntentsResponse) {
+				suite.Require().Equal(len(intents), 2)
+				suite.Require().Equal(intents[0].ChainId, "cosmoshub-4")
+				suite.Require().Equal(intents[1].ChainId, suite.chainB.ChainID)
+				for _, intent := range intents {
+					suite.Require().Equal(intent.Intent.Delegator, testAddress)
+					suite.Require().Equal(len(intent.Intent.Intents), 1)
+				}
+			},
+		},
+	}
+
+	// run tests:
+	suite.setupTestZones()
+
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			tt.malleate()
+			resp, err := icsKeeper.DelegatorIntents(
+				ctx,
+				tt.req,
+			)
+			if tt.wantErr {
+				suite.T().Logf("Error:\n%v\n", err)
+				suite.Require().Error(err)
+				return
+			}
+			suite.Require().NoError(err)
+			suite.Require().NotNil(resp)
+			tt.verify(resp.Intents)
+
+			vstr, err := json.MarshalIndent(resp, "", "\t")
+			suite.Require().NoError(err)
+
+			suite.T().Logf("Response:\n%s\n", vstr)
+		})
+	}
+}
+
 func (suite *KeeperTestSuite) TestKeeper_Delegations() {
 	icsKeeper := suite.GetQuicksilverApp(suite.chainA).InterchainstakingKeeper
 	ctx := suite.chainA.GetContext()
@@ -525,7 +657,7 @@ func (suite *KeeperTestSuite) TestKeeper_ZoneWithdrawalRecords() {
 			func() {},
 			&types.QueryWithdrawalRecordsRequest{
 				ChainId:          suite.chainB.ChainID,
-				DelegatorAddress: "quick16pxh2v4hr28h2gkntgfk8qgh47pfmjfhzgeure",
+				DelegatorAddress: delegatorAddress,
 			},
 			false,
 			0,
@@ -551,7 +683,7 @@ func (suite *KeeperTestSuite) TestKeeper_ZoneWithdrawalRecords() {
 				icsKeeper.AddWithdrawalRecord(
 					ctx,
 					zone.ChainId,
-					"quick16pxh2v4hr28h2gkntgfk8qgh47pfmjfhzgeure",
+					delegatorAddress,
 					distribution,
 					testAddress,
 					sdk.NewCoins(sdk.NewCoin(zone.BaseDenom, math.NewInt(15000000))),
@@ -563,7 +695,7 @@ func (suite *KeeperTestSuite) TestKeeper_ZoneWithdrawalRecords() {
 			},
 			&types.QueryWithdrawalRecordsRequest{
 				ChainId:          suite.chainB.ChainID,
-				DelegatorAddress: "quick16pxh2v4hr28h2gkntgfk8qgh47pfmjfhzgeure",
+				DelegatorAddress: delegatorAddress,
 			},
 			false,
 			1,
@@ -655,7 +787,7 @@ func (suite *KeeperTestSuite) TestKeeper_UserWithdrawalRecords() {
 				icsKeeper.AddWithdrawalRecord(
 					ctx,
 					zone.ChainId,
-					"quick16pxh2v4hr28h2gkntgfk8qgh47pfmjfhzgeure",
+					delegatorAddress,
 					distribution,
 					testAddress,
 					sdk.NewCoins(sdk.NewCoin(zone.BaseDenom, math.NewInt(15000000))),
@@ -666,7 +798,7 @@ func (suite *KeeperTestSuite) TestKeeper_UserWithdrawalRecords() {
 				)
 			},
 			&types.QueryUserWithdrawalRecordsRequest{
-				UserAddress: "quick16pxh2v4hr28h2gkntgfk8qgh47pfmjfhzgeure",
+				UserAddress: delegatorAddress,
 			},
 			false,
 			1,
@@ -747,7 +879,7 @@ func (suite *KeeperTestSuite) TestKeeper_WithdrawalRecords() {
 				icsKeeper.AddWithdrawalRecord(
 					ctx,
 					zone.ChainId,
-					"quick16pxh2v4hr28h2gkntgfk8qgh47pfmjfhzgeure",
+					delegatorAddress,
 					distribution,
 					testAddress,
 					sdk.NewCoins(sdk.NewCoin(zone.BaseDenom, math.NewInt(15000000))),
