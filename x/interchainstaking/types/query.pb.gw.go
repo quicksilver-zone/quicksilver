b27709887e35f76f96f99120ccec3dce2bd47c04
Muku <44918265+muku314115@users.noreply.github.com>
2023-06-25 17:55:08 +0530

    test: endpoint to get the receipt for a txHash (#469)
    * front-port v1.2.13
    
    * fix delegation flush test
    
    * add tests for HandleDepositTransaction
    
    * update tests for new error handling
    
    * remove superflous field in proto
    
    * final v1.2.13 front-port and test fixes
    
    * s/s/suite/
    
    * remove fmt.Printlns used for debugging
    
    * update participation rewards and protocoldatas, to add validation, automatic key generation, basechain/basedenom in osmosisparams and updated tvl calculation
    
    * front-port v1.2.13
    
    * remove balancerPoolPretty json marshal/unmarshal, as we deal with raw KV store objects; switch to native bech32 decode/encode as we dont expect prefix of pools to match sdk.GetConfig().Bech32Prefix
    
    * fix: allow support for multiple denoms from same zone in tvl calc
    
    * lint
    
    * fix: refactor utils/address.go to avoid import cycle when using in osmosis-types
    
    * add test for randomutils
    
    * fix: remove potential non-det by iterating over maps using sorted keys
    
    * resolve outstanding nits
    
    * lint
    
    * fix nits
    
    * bump go to 1.20.5
    
    * Update x/interchainstaking/keeper/receipt.go
    
    * Apply suggestions from code review
    
    * lint
    
    * bump hermes to v1.5.0
    
    * update icq and relayer in test scripts
    
    * initial changes
    
    * ensuring return
    
    * tests
    
    * tests
    
    * tests
    
    * tests
    
    * changing path name
    
    * lint
    
    * proto format
    
    * typo
    
    * updating tests
    
    * proto format
    
    ---------
    
    Co-authored-by: Joe Bowman <joe@ingenuity.build>
    Co-authored-by: Ajaz Ahmed Ansari <ajansari9585@gmail.com>

--- x/interchainstaking/types/query.pb.gw.go
+++ x/interchainstaking/types/query.pb.gw.go
@@ -523,6 +523,82 @@ func local_request_Query_Receipts_0(ctx context.Context, marshaler runtime.Marsh
 
 }
 
+func request_Query_TxStatus_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryTxStatusRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["chain_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "chain_id")
+	}
+
+	protoReq.ChainId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "chain_id", err)
+	}
+
+	val, ok = pathParams["tx_hash"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "tx_hash")
+	}
+
+	protoReq.TxHash, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "tx_hash", err)
+	}
+
+	msg, err := client.TxStatus(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Query_TxStatus_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryTxStatusRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["chain_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "chain_id")
+	}
+
+	protoReq.ChainId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "chain_id", err)
+	}
+
+	val, ok = pathParams["tx_hash"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "tx_hash")
+	}
+
+	protoReq.TxHash, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "tx_hash", err)
+	}
+
+	msg, err := server.TxStatus(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 var (
 	filter_Query_ZoneWithdrawalRecords_0 = &utilities.DoubleArray{Encoding: map[string]int{"chain_id": 0, "delegator_address": 1}, Base: []int{1, 1, 2, 0, 0}, Check: []int{0, 1, 1, 2, 3}}
 )
@@ -1167,6 +1243,29 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 
 	})
 
+	mux.Handle("GET", pattern_Query_TxStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Query_TxStatus_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_TxStatus_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_Query_ZoneWithdrawalRecords_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1506,6 +1605,26 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
+	mux.Handle("GET", pattern_Query_TxStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Query_TxStatus_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Query_TxStatus_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_Query_ZoneWithdrawalRecords_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1646,6 +1765,8 @@ var (
 
 	pattern_Query_Receipts_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"quicksilver", "interchainstaking", "v1", "zones", "chain_id", "receipts"}, "", runtime.AssumeColonVerbOpt(true)))
 
+	pattern_Query_TxStatus_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5, 1, 0, 4, 1, 5, 6}, []string{"quicksilver", "interchainstaking", "v1", "zones", "chain_id", "deposits", "tx_hash"}, "", runtime.AssumeColonVerbOpt(true)))
+
 	pattern_Query_ZoneWithdrawalRecords_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5, 1, 0, 4, 1, 5, 6}, []string{"quicksilver", "interchainstaking", "v1", "zones", "chain_id", "withdrawal_records", "delegator_address"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Query_WithdrawalRecords_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"quicksilver", "interchainstaking", "v1", "zones", "chain_id", "withdrawal_records"}, "", runtime.AssumeColonVerbOpt(true)))
@@ -1675,6 +1796,8 @@ var (
 	forward_Query_Delegations_0 = runtime.ForwardResponseMessage
 
 	forward_Query_Receipts_0 = runtime.ForwardResponseMessage
+
+	forward_Query_TxStatus_0 = runtime.ForwardResponseMessage
 
 	forward_Query_ZoneWithdrawalRecords_0 = runtime.ForwardResponseMessage
 
