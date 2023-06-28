80c2e61e6adb93f898239cbe7e173ef243d69c57
muku314115 <mukangotan@gmail.com>
2023-06-28 17:06:52 +0530

    callbacks

--- x/participationrewards/keeper/callbacks.go
+++ x/participationrewards/keeper/callbacks.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	umeetypes "github.com/ingenuity-build/quicksilver/umee/types"
 
 	"github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,6 +13,16 @@ import (
 	"github.com/ingenuity-build/quicksilver/osmosis-types/gamm"
 	icqtypes "github.com/ingenuity-build/quicksilver/x/interchainquery/types"
 	"github.com/ingenuity-build/quicksilver/x/participationrewards/types"
+)
+
+const (
+	ValidatorSelectionRewardsCallbackID = "validatorselectionrewards"
+	OsmosisPoolUpdateCallbackID         = "osmosispoolupdate"
+	SetEpochBlockCallbackID             = "epochblock"
+	UmeeReservesUpdateCallbackID        = "umeereservesupdatecallback"
+	UmeeTotalBorrowsUpdateCallbackID    = "umeetotalborrowsupdatecallback"
+	UmeeInterestScalarUpdateCallbackID  = "umeeinterestscalarupdatecallback"
+	UmeeUTokenSupplyCallbackID          = "umeeutokensupplycallback"
 )
 
 // Callback wrapper struct for interchainstaking keeper.
@@ -47,7 +58,11 @@ func (c Callbacks) RegisterCallbacks() icqtypes.QueryCallbacks {
 	a := c.
 		AddCallback("validatorselectionrewards", Callback(ValidatorSelectionRewardsCallback)).
 		AddCallback("osmosispoolupdate", Callback(OsmosisPoolUpdateCallback)).
-		AddCallback("epochblock", Callback(SetEpochBlockCallback))
+		AddCallback("epochblock", Callback(SetEpochBlockCallback)).
+		AddCallback(UmeeReservesUpdateCallbackID, Callback(UmeeReservesUpdateCallback)).
+		AddCallback(UmeeTotalBorrowsUpdateCallbackID, Callback(UmeeTotalBorrowsUpdateCallback)).
+		AddCallback(UmeeInterestScalarUpdateCallbackID, Callback(UmeeInterestScalarUpdateCallback)).
+		AddCallback(UmeeUTokenSupplyCallbackID, Callback(UmeeUTokenSupplyCallback))
 
 	return a.(Callbacks)
 }
@@ -138,6 +153,146 @@ func OsmosisPoolUpdateCallback(ctx sdk.Context, k *Keeper, response []byte, quer
 
 	return nil
 }
+
+func UmeeReservesUpdateCallback(ctx sdk.Context, k *Keeper, response []byte, query icqtypes.Query) error {
+	reserveAmount := sdk.ZeroInt()
+	if err := k.cdc.UnmarshalInterface(response, &reserveAmount); err != nil {
+		return err
+	}
+	key := query.Request[1:]
+	denom := umeetypes.DenomFromKey(key, umeetypes.KeyPrefixReserveAmount)
+	data, ok := k.GetProtocolData(ctx, types.ProtocolDataTypeUmeeReserves, denom)
+	if !ok {
+		return fmt.Errorf("unable to find protocol data for umeereserves/%s", denom)
+	}
+	ireserves, err := types.UnmarshalProtocolData(types.ProtocolDataTypeUmeeReserves, data.Data)
+	if err != nil {
+		return err
+	}
+	reserves, ok := ireserves.(*types.UmeeReservesProtocolData)
+	if !ok {
+		return fmt.Errorf("unable to unmarshal protocol data for umeereserves/%s", denom)
+	}
+	reserves.Data, err = json.Marshal(reserveAmount)
+	if err != nil {
+		return err
+	}
+	reserves.LastUpdated = ctx.BlockTime()
+	data.Data, err = json.Marshal(reserves)
+	if err != nil {
+		return err
+	}
+	k.SetProtocolData(ctx, reserves.GenerateKey(), &data)
+
+	return nil
+}
+
+func UmeeTotalBorrowsUpdateCallback(ctx sdk.Context, k *Keeper, response []byte, query icqtypes.Query) error {
+	totalBorrows := sdk.ZeroDec()
+	if err := k.cdc.UnmarshalInterface(response, &totalBorrows); err != nil {
+		return err
+	}
+
+	key := query.Request[1:]
+	denom := umeetypes.DenomFromKey(key, umeetypes.KeyPrefixAdjustedTotalBorrow)
+	data, ok := k.GetProtocolData(ctx, types.ProtocolDataTypeUmeeTotalBorrows, denom)
+	if !ok {
+		return fmt.Errorf("unable to find protocol data for umeereserves/%s", denom)
+	}
+	iborrows, err := types.UnmarshalProtocolData(types.ProtocolDataTypeUmeeTotalBorrows, data.Data)
+	if err != nil {
+		return err
+	}
+	borrows, ok := iborrows.(*types.UmeeTotalBorrowsProtocolData)
+	if !ok {
+		return fmt.Errorf("unable to unmarshal protocol data for umeereserves/%s", denom)
+	}
+	borrows.Data, err = json.Marshal(totalBorrows)
+	if err != nil {
+		return err
+	}
+	borrows.LastUpdated = ctx.BlockTime()
+	data.Data, err = json.Marshal(borrows)
+	if err != nil {
+		return err
+	}
+	k.SetProtocolData(ctx, borrows.GenerateKey(), &data)
+
+	return nil
+}
+
+func UmeeInterestScalarUpdateCallback(ctx sdk.Context, k *Keeper, response []byte, query icqtypes.Query) error {
+	interestScalar := sdk.ZeroDec()
+	if err := k.cdc.UnmarshalInterface(response, &interestScalar); err != nil {
+		return err
+	}
+
+	key := query.Request[1:]
+	denom := umeetypes.DenomFromKey(key, umeetypes.KeyPrefixInterestScalar)
+	data, ok := k.GetProtocolData(ctx, types.ProtocolDataTypeUmeeInterestScalar, denom)
+	if !ok {
+		return fmt.Errorf("unable to find protocol data for interestscalar/%s", denom)
+	}
+	iinterest, err := types.UnmarshalProtocolData(types.ProtocolDataTypeUmeeInterestScalar, data.Data)
+	if err != nil {
+		return err
+	}
+	interest, ok := iinterest.(*types.UmeeInterestScalarProtocolData)
+	if !ok {
+		return fmt.Errorf("unable to unmarshal protocol data for interestscalar/%s", denom)
+	}
+	interest.Data, err = json.Marshal(interestScalar)
+	if err != nil {
+		return err
+	}
+	interest.LastUpdated = ctx.BlockTime()
+	data.Data, err = json.Marshal(interest)
+	if err != nil {
+		return err
+	}
+	k.SetProtocolData(ctx, interest.GenerateKey(), &data)
+
+	return nil
+}
+
+func UmeeUTokenSupplyCallback(ctx sdk.Context, k *Keeper, response []byte, query icqtypes.Query) error {
+	supplyAmount := sdk.ZeroInt()
+	if err := k.cdc.UnmarshalInterface(response, &supplyAmount); err != nil {
+		return err
+	}
+	key := query.Request[1:]
+	denom := umeetypes.DenomFromKey(key, umeetypes.KeyPrefixReserveAmount)
+	data, ok := k.GetProtocolData(ctx, types.ProtocolDataTypeUmeeUTokenSupply, denom)
+	if !ok {
+		return fmt.Errorf("unable to find protocol data for umeereserves/%s", denom)
+	}
+	isupply, err := types.UnmarshalProtocolData(types.ProtocolDataTypeUmeeUTokenSupply, data.Data)
+	if err != nil {
+		return err
+	}
+	supply, ok := isupply.(*types.UmeeUTokenSupplyProtocolData)
+	if !ok {
+		return fmt.Errorf("unable to unmarshal protocol data for umeereserves/%s", denom)
+	}
+	supply.Data, err = json.Marshal(supplyAmount)
+	if err != nil {
+		return err
+	}
+	supply.LastUpdated = ctx.BlockTime()
+	data.Data, err = json.Marshal(supply)
+	if err != nil {
+		return err
+	}
+	k.SetProtocolData(ctx, supply.GenerateKey(), &data)
+
+	return nil
+}
+
+//umee bank balances for a denom callback
+//umee module balance callback
+//umee collateral callback
+//umee reserves callback
+//as long as we have the above two, we ill know what the user's total balance is in u/asset and we may need these values for reward calculation
 
 // SetEpochBlockCallback records the block height of the registered zone at the epoch boundary.
 func SetEpochBlockCallback(ctx sdk.Context, k *Keeper, args []byte, query icqtypes.Query) error {
