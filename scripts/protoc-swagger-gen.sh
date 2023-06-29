f7ed25f1a7bcd8e6020b91659136b3de186b32d7
anhductn2001 <ntd1312000@gmail.com>
2023-06-29 12:42:51 +0700

    proto-gen-swagger

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -7,10 +7,9 @@ cd proto
 proto_dirs=$(find ./quicksilver -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
-  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
-  if [[ ! -z "$query_file" ]]; then
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     buf generate --template buf.gen.swagger.yaml $query_file
-  fi
+  done
 done
 
 cd ..
