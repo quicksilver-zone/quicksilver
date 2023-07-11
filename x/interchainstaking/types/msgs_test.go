84628ae031cbdaf9a41f43f394785b17b7355b4a
aljo242 <alex@shmeeload.xyz>
2023-07-11 10:19:56 -0400

    working

--- x/interchainstaking/types/msgs_test.go
+++ x/interchainstaking/types/msgs_test.go
@@ -11,6 +11,263 @@ import (
 	"github.com/ingenuity-build/quicksilver/x/interchainstaking/types"
 )
 
+func TestMsgRegisterZone_ValidateBasic(t *testing.T) {
+	testAddress := addressutils.GenerateAccAddressForTest().String()
+
+	type fields struct {
+		Title            string
+		Description      string
+		ConnectionID     string
+		BaseDenom        string
+		LocalDenom       string
+		AccountPrefix    string
+		ReturnToSender   bool
+		UnbondingEnabled bool
+		Deposits         bool
+		LiquidityModule  bool
+		Decimals         int64
+		MessagesPerTx    int64
+		Is118            bool
+		SubzoneInfo      *types.SubzoneInfo
+		Authority        string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "test valid",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "uatom",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         6,
+				MessagesPerTx:    5,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid gov content",
+			fields: fields{
+				Title:            "",
+				Description:      "",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "uatom",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         6,
+				MessagesPerTx:    5,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid connection field",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "test",
+				BaseDenom:        "uatom",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         6,
+				MessagesPerTx:    5,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid basedenom field",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "0",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         6,
+				MessagesPerTx:    5,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid localdenom field",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "uatom",
+				LocalDenom:       "0",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         6,
+				MessagesPerTx:    5,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid account prefix",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "uatom",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "a",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         6,
+				MessagesPerTx:    5,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+		{
+			name: "liquidity",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "uatom",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  true,
+				Decimals:         6,
+				MessagesPerTx:    5,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid decimals",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "uatom",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         0,
+				MessagesPerTx:    5,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid-messages-per-tx-0",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "uatom",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         0,
+				MessagesPerTx:    0,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid-messages-per-tx-negative",
+			fields: fields{
+				Title:            "Enable testzone-1",
+				Description:      "onboard testzone-1",
+				ConnectionID:     "connection-0",
+				BaseDenom:        "uatom",
+				LocalDenom:       "uqatom",
+				AccountPrefix:    "cosmos",
+				ReturnToSender:   false,
+				UnbondingEnabled: false,
+				Deposits:         false,
+				LiquidityModule:  false,
+				Decimals:         0,
+				MessagesPerTx:    -1,
+				Is118:            true,
+				Authority:        testAddress,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := types.MsgRegisterZone{
+				Title:            tt.fields.Title,
+				Description:      tt.fields.Description,
+				ConnectionId:     tt.fields.ConnectionID,
+				BaseDenom:        tt.fields.BaseDenom,
+				LocalDenom:       tt.fields.LocalDenom,
+				AccountPrefix:    tt.fields.AccountPrefix,
+				LiquidityModule:  tt.fields.LiquidityModule,
+				MessagesPerTx:    tt.fields.MessagesPerTx,
+				ReturnToSender:   tt.fields.ReturnToSender,
+				DepositsEnabled:  tt.fields.Deposits,
+				UnbondingEnabled: tt.fields.UnbondingEnabled,
+				Decimals:         tt.fields.Decimals,
+				Is_118:           tt.fields.Is118,
+				// SubzoneInfo:      tt.fields.,
+				Authority: tt.fields.Authority,
+			}
+
+			err := m.ValidateBasic()
+			if tt.wantErr {
+				t.Logf("Error:\n%v\n", err)
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
+
 func TestIntentsFromString(t *testing.T) {
 	// 1. Ensure we can properly parse intents with their weights.
 	intents := "0.3cosmosvaloper1sjllsnramtg3ewxqwwrwjxfgc4n4ef9u2lcnj0,0.3cosmosvaloper156gqf9837u7d4c4678yt3rl4ls9c5vuursrrzf,0.4cosmosvaloper1a3yjj7d3qnx4spgvjcwjq9cw9snrrrhu5h6jll"
