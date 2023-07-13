01e9f2d66f498d0df300747bc5d98dc6ae7e791c
Ajaz Ahmed Ansari <ajansari9585@gmail.com>
2023-07-13 21:14:43 +0530

    Update x/interchainstaking/keeper/delegation.go

--- x/interchainstaking/keeper/delegation.go
+++ x/interchainstaking/keeper/delegation.go
@@ -282,7 +282,6 @@ func (k *Keeper) FlushOutstandingDelegations(ctx sdk.Context, zone *types.Zone, 
 	exclusionTime := ctx.BlockTime().AddDate(0, 0, -1)
 	var coinsToFlush sdk.Coins
 	k.IterateZoneReceipts(ctx, zone, func(_ int64, receiptInfo types.Receipt) (stop bool) {
-		// fmt.Println(receiptInfo)
 		if (receiptInfo.FirstSeen.After(exclusionTime) || receiptInfo.FirstSeen.Equal(exclusionTime)) && receiptInfo.Completed == nil {
 			pendingAmount = pendingAmount.Add(receiptInfo.Amount...)
 		}
