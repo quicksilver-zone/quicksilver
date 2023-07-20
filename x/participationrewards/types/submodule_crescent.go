e171da89ea07366cd4b74426fe16952a1edff1a1
Joe Bowman <joe@ingenuity.build>
2023-07-20 20:17:08 +0100

    fix conflict with externalised multierror mod

--- x/participationrewards/types/submodule_crescent.go
+++ x/participationrewards/types/submodule_crescent.go
@@ -12,7 +12,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/ingenuity-build/quicksilver/internal/multierror"
+	"github.com/ingenuity-build/multierror"
 	"github.com/ingenuity-build/quicksilver/utils"
 )
 
