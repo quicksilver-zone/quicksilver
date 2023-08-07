7708327b64bcf7e3045c4ef0c3fa66debddeada7
Muku <44918265+muku314115@users.noreply.github.com>
2023-08-08 02:10:49 +0530

    buf fixes for crescent address decoding and test file updates (#524)

--- third-party-chains/crescent-types/liquidity/types/pool.go
+++ third-party-chains/crescent-types/liquidity/types/pool.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/ingenuity-build/quicksilver/utils/addressutils"
 )
 
-func (pool Pool) GetReserveAddress() sdk.AccAddress {
-	addr, err := sdk.AccAddressFromBech32(pool.ReserveAddress)
-	if err != nil {
-		panic(err)
-	}
-	return addr
+func (pool Pool) GetReserveAddress() string {
+	return pool.ReserveAddress
 }
 
 // Validate validates Pool for genesis.
@@ -22,7 +20,7 @@ func (pool Pool) Validate() error {
 	if pool.PairId == 0 {
 		return fmt.Errorf("pair id must not be 0")
 	}
-	if _, err := sdk.AccAddressFromBech32(pool.ReserveAddress); err != nil {
+	if _, err := addressutils.AccAddressFromBech32(pool.ReserveAddress, ""); err != nil {
 		return fmt.Errorf("invalid reserve address %s: %w", pool.ReserveAddress, err)
 	}
 	if err := sdk.ValidateDenom(pool.PoolCoinDenom); err != nil {
