9be83fa5323655fc14dc1be6a14cc2da23c2f1ed
Jacob Gadikian <jacobgadikian@gmail.com>
2023-09-16 00:33:11 +0800

    finish linting (#568)

--- wasmbinding/query_plugin_test.go
+++ wasmbinding/query_plugin_test.go
@@ -227,7 +227,7 @@ func (s *StargateTestSuite) TestDeterministicJsonMarshal() {
 		responseProtoStruct interface{}
 		expectedProto       func() proto.Message
 	}{
-		/**
+		/*
 		   * Origin Response
 		   * balances:<denom:"bar" amount:"30" > pagination:<next_key:"foo" >
 		   * New Version Response
@@ -274,7 +274,7 @@ func (s *StargateTestSuite) TestDeterministicJsonMarshal() {
 				}
 			},
 		},
-		/**
+		/*
 		  // Origin proto
 		  message QueryAccountResponse {
 		    // account defines the account of the corresponding address.
