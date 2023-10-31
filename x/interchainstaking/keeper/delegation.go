3926fd6376e1494a97e345d0df845d28a636d4c1
DongLieu <duongduydongtq@gmail.com>
2023-10-31 08:31:06 +0700

    add RemovePertDelegation and change logic for RemoveZoneAndAssociatedRecords

--- x/interchainstaking/keeper/delegation.go
+++ x/interchainstaking/keeper/delegation.go
@@ -168,6 +168,15 @@ func (k *Keeper) RemoveDelegation(ctx sdk.Context, chainID string, delegation ty
 	return nil
 }
 
+// RemovePerformanceDelegation removes a performance delegation.
+func (k *Keeper) RemovePerformanceDelegation(ctx sdk.Context, chainID string, delegation types.Delegation) error {
+	delegatorAddress := delegation.GetDelegatorAddr()
+
+	store := ctx.KVStore(k.storeKey)
+	store.Delete(types.GetPerformanceDelegationKey(chainID, delegatorAddress, delegation.GetValidatorAddr()))
+	return nil
+}
+
 // IterateDelegatorDelegations iterates through one delegator's delegations.
 func (k *Keeper) IterateDelegatorDelegations(ctx sdk.Context, chainID string, delegator sdk.AccAddress, cb func(delegation types.Delegation) (stop bool)) {
 	store := ctx.KVStore(k.storeKey)
