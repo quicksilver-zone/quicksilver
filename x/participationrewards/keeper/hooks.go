f4ca7430f0d99561867d795eb249db053d6b6c6d
Tien Dung <neitdung@gmail.com>
2023-11-02 17:49:27 +0700

    early return long function

--- x/participationrewards/keeper/hooks.go
+++ x/participationrewards/keeper/hooks.go
@@ -16,78 +16,80 @@ func (*Keeper) BeforeEpochStart(_ sdk.Context, _ string, _ int64) error {
 }
 
 func (k *Keeper) AfterEpochEnd(ctx sdk.Context, epochIdentifier string, _ int64) error {
-	if epochIdentifier == epochstypes.EpochIdentifierEpoch {
-		k.IteratePrefixedProtocolDatas(ctx, types.GetPrefixProtocolDataKey(types.ProtocolDataTypeConnection), func(index int64, _ []byte, data types.ProtocolData) (stop bool) {
-			blockQuery := tmservice.GetLatestBlockRequest{}
-			bz := k.cdc.MustMarshal(&blockQuery)
+	if epochIdentifier != epochstypes.EpochIdentifierEpoch {
+		return nil
+	}
+	
+	k.IteratePrefixedProtocolDatas(ctx, types.GetPrefixProtocolDataKey(types.ProtocolDataTypeConnection), func(index int64, _ []byte, data types.ProtocolData) (stop bool) {
+		blockQuery := tmservice.GetLatestBlockRequest{}
+		bz := k.cdc.MustMarshal(&blockQuery)
 
-			iConnectionData, err := types.UnmarshalProtocolData(types.ProtocolDataTypeConnection, data.Data)
-			if err != nil {
-				k.Logger(ctx).Error("Error unmarshalling protocol data")
-			}
-			connectionData, _ := iConnectionData.(*types.ConnectionProtocolData)
-			if connectionData.ChainID == ctx.ChainID() {
-				return false
-			}
-
-			k.IcqKeeper.MakeRequest(
-				ctx,
-				connectionData.ConnectionID,
-				connectionData.ChainID,
-				"cosmos.base.tendermint.v1beta1.Service/GetLatestBlock",
-				bz,
-				sdk.NewInt(-1),
-				types.ModuleName,
-				SetEpochBlockCallbackID,
-				0,
-			)
+		iConnectionData, err := types.UnmarshalProtocolData(types.ProtocolDataTypeConnection, data.Data)
+		if err != nil {
+			k.Logger(ctx).Error("Error unmarshalling protocol data")
+		}
+		connectionData, _ := iConnectionData.(*types.ConnectionProtocolData)
+		if connectionData.ChainID == ctx.ChainID() {
 			return false
-		})
-
-		k.Logger(ctx).Info("setting self connection data...")
-		err := k.UpdateSelfConnectionData(ctx)
-		if err != nil {
-			panic(err)
 		}
 
-		k.Logger(ctx).Info("distribute participation rewards...")
-
-		allocation, err := types.GetRewardsAllocations(
-			k.GetModuleBalance(ctx),
-			k.GetParams(ctx).DistributionProportions,
+		k.IcqKeeper.MakeRequest(
+			ctx,
+			connectionData.ConnectionID,
+			connectionData.ChainID,
+			"cosmos.base.tendermint.v1beta1.Service/GetLatestBlock",
+			bz,
+			sdk.NewInt(-1),
+			types.ModuleName,
+			SetEpochBlockCallbackID,
+			0,
 		)
-		if err != nil {
+		return false
+	})
+
+	k.Logger(ctx).Info("setting self connection data...")
+	err := k.UpdateSelfConnectionData(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	k.Logger(ctx).Info("distribute participation rewards...")
+
+	allocation, err := types.GetRewardsAllocations(
+		k.GetModuleBalance(ctx),
+		k.GetParams(ctx).DistributionProportions,
+	)
+	if err != nil {
+		k.Logger(ctx).Error(err.Error())
+	}
+
+	k.Logger(ctx).Info("Triggering submodule hooks")
+	for _, sub := range k.prSubmodules {
+		sub.Hooks(ctx, k)
+	}
+
+	tvs, err := k.CalcTokenValues(ctx)
+	if err != nil {
+		k.Logger(ctx).Error("unable to calculate token values", "error", err.Error())
+		return nil
+	}
+
+	if allocation == nil {
+		// if allocation is unset, then return early to avoid panic
+		k.Logger(ctx).Error("nil allocation", "error", err.Error())
+		return nil
+	}
+
+	if err := k.AllocateZoneRewards(ctx, tvs, *allocation); err != nil {
+		k.Logger(ctx).Error("unable to allocate: tvl is zero", "error", err.Error())
+		return nil
+	}
+
+	if !allocation.Lockup.IsZero() {
+		// at genesis lockup will be disabled, and enabled when ICS is used.
+		if err := k.AllocateLockupRewards(ctx, allocation.Lockup); err != nil {
 			k.Logger(ctx).Error(err.Error())
-		}
-
-		k.Logger(ctx).Info("Triggering submodule hooks")
-		for _, sub := range k.prSubmodules {
-			sub.Hooks(ctx, k)
-		}
-
-		tvs, err := k.CalcTokenValues(ctx)
-		if err != nil {
-			k.Logger(ctx).Error("unable to calculate token values", "error", err.Error())
-			return nil
-		}
-
-		if allocation == nil {
-			// if allocation is unset, then return early to avoid panic
-			k.Logger(ctx).Error("nil allocation", "error", err.Error())
-			return nil
-		}
-
-		if err := k.AllocateZoneRewards(ctx, tvs, *allocation); err != nil {
-			k.Logger(ctx).Error("unable to allocate: tvl is zero", "error", err.Error())
-			return nil
-		}
-
-		if !allocation.Lockup.IsZero() {
-			// at genesis lockup will be disabled, and enabled when ICS is used.
-			if err := k.AllocateLockupRewards(ctx, allocation.Lockup); err != nil {
-				k.Logger(ctx).Error(err.Error())
-				return err
-			}
+			return err
 		}
 	}
 	return nil
