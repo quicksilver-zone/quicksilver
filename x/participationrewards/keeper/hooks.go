ea4aa343abc8e7aa6f8c6859355a86d3a495727a
Tien Dung <neitdung@gmail.com>
2023-11-02 17:54:05 +0700

    fix spacing hook

--- x/participationrewards/keeper/hooks.go
+++ x/participationrewards/keeper/hooks.go
@@ -19,7 +19,7 @@ func (k *Keeper) AfterEpochEnd(ctx sdk.Context, epochIdentifier string, _ int64)
 	if epochIdentifier != epochstypes.EpochIdentifierEpoch {
 		return nil
 	}
-	
+
 	k.IteratePrefixedProtocolDatas(ctx, types.GetPrefixProtocolDataKey(types.ProtocolDataTypeConnection), func(index int64, _ []byte, data types.ProtocolData) (stop bool) {
 		blockQuery := tmservice.GetLatestBlockRequest{}
 		bz := k.cdc.MustMarshal(&blockQuery)
