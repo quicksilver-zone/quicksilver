a38f8f00e79bfc4e9c4160f12b95827fb51c7a00
Joe Bowman <joe@ingenuity.build>
2023-11-30 14:17:14 +0000

    fix: improve performance of lsm share token validation; fixes #787

--- utils/strings.go
+++ utils/strings.go
@@ -8,3 +8,12 @@ func StringSliceToMap(in []string) map[string]bool {
 	}
 	return out
 }
+
+// create a map from a slice of strings for efficient lookup
+func StringSliceToMap(in []string) map[string]bool {
+	out := make(map[string]bool, len(in))
+	for _, i := range in {
+		out[i] = true
+	}
+	return out
+}
