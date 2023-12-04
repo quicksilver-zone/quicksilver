4fcd8b4561c280dcccfc2ed1c419489ced5f5dfd
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2023-12-04 12:34:12 -0700

    ts ignore

--- next.config.js
+++ next.config.js
@@ -5,4 +5,11 @@ module.exports = {
   swcMinify: true,
   basePath: '/quicksilver-app-v2',
   assetPrefix: '/quicksilver-app-v2',
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
