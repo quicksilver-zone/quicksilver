88f3c5c280decb3521ba934a0f8e1ff788c7aca3
Jacob Gadikian <jacobgadikian@gmail.com>
2023-12-20 04:10:25 +0700

    remove all web related components

--- quicksilverjs/src/codegen/quicksilver/participationrewards/v1/messages.amino.ts
+++
@@ -1,9 +0,0 @@
-import { ClaimType, ClaimTypeSDKType, Proof, ProofSDKType } from "../../claimsmanager/v1/claimsmanager";
-import { MsgSubmitClaim, MsgSubmitClaimSDKType } from "./messages";
-export const AminoConverter = {
-  "/quicksilver.participationrewards.v1.MsgSubmitClaim": {
-    aminoType: "/quicksilver.participationrewards.v1.MsgSubmitClaim",
-    toAmino: MsgSubmitClaim.toAmino,
-    fromAmino: MsgSubmitClaim.fromAmino
-  }
-};
