dc7847dcc66add811156fd48ad095e1f5fe30ef4
Tuan Tran <tuantran@notional.ventures>
2024-01-17 17:38:01 +0700

    fix(cmd): add additional err return

--- cmd/quicksilverd/main.go
+++ cmd/quicksilverd/main.go
@@ -22,7 +22,7 @@ func main() {
 	}
 	app.DefaultNodeHome = filepath.Join(userHomeDir, ".quicksilverd")
 
-	rootCmd := cmd.NewRootCmd()
+	rootCmd, _ := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
 		log.NewLogger(rootCmd.OutOrStderr()).Error("failure when running app", "err", err)
 		os.Exit(1)
