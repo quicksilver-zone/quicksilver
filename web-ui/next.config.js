89e8ef2d4400e8287d262449c9edf78e4d31654a
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2024-01-17 00:08:57 -0700

    add cosmjs override

--- web-ui/next.config.js
+++ web-ui/next.config.js
@@ -17,7 +17,7 @@ module.exports = {
   typescript: {
     // !! WARN !! //
     // There are no fatal errors in this project, this option is used as a workaround due to the amalgamation of packages we are using //
-    // This option will be removed once all dependencies are updated to use the lates versions //
+    // This option will be removed once all dependencies are updated to use the latest versions //
     ignoreBuildErrors: true,
   },
 };
