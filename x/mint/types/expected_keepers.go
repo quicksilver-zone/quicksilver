a1653bc6d7aebb80277fd2ca47a266f1af8b9a63
Tuan Tran <tuantran@notional.ventures>
2024-01-18 11:31:39 +0700

    fix(module): refactor expected keeper to use correct parameter

--- x/mint/types/expected_keepers.go
+++ x/mint/types/expected_keepers.go
@@ -1,8 +1,9 @@
 package types // noalias
 
 import (
+	context "context"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/types"
 
 	epochstypes "github.com/quicksilver-zone/quicksilver/v7/x/epochs/types"
 )
@@ -10,30 +11,30 @@ import (
 // AccountKeeper defines the contract required for account APIs.
 type AccountKeeper interface {
 	GetModuleAddress(name string) sdk.AccAddress
-	HasAccount(ctx sdk.Context, addr sdk.AccAddress) bool
+	HasAccount(ctx context.Context, addr sdk.AccAddress) bool
 
 	// TODO remove with genesis 2-phases refactor https://github.com/cosmos/cosmos-sdk/issues/2862
-	SetModuleAccount(sdk.Context, types.ModuleAccountI)
-	GetModuleAccount(ctx sdk.Context, moduleName string) types.ModuleAccountI
+	SetModuleAccount(context.Context, sdk.ModuleAccountI)
+	GetModuleAccount(ctx context.Context, moduleName string) sdk.ModuleAccountI
 }
 
 // BankKeeper defines the contract needed to be fulfilled for banking and supply
 // dependencies.
 type BankKeeper interface {
-	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
-	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
-	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
-	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
-	BurnCoins(ctx sdk.Context, name string, amt sdk.Coins) error
-	// AddSupplyOffset(ctx sdk.Context, denom string, offsetAmount sdkmath.Int)
+	GetBalance(ctx context.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromModuleToModule(ctx context.Context, senderModule, recipientModule string, amt sdk.Coins) error
+	MintCoins(ctx context.Context, name string, amt sdk.Coins) error
+	BurnCoins(ctx context.Context, name string, amt sdk.Coins) error
+	// AddSupplyOffset(ctx context.Context, denom string, offsetAmount sdkmath.Int)
 }
 
 // DistrKeeper defines the contract needed to be fulfilled for distribution keeper.
 type DistrKeeper interface {
-	FundCommunityPool(ctx sdk.Context, amount sdk.Coins, sender sdk.AccAddress) error
+	FundCommunityPool(ctx context.Context, amount sdk.Coins, sender sdk.AccAddress) error
 }
 
 // EpochKeeper defines the contract needed to be fulfilled for epochs keeper.
 type EpochKeeper interface {
-	GetEpochInfo(ctx sdk.Context, identifier string) epochstypes.EpochInfo
+	GetEpochInfo(ctx context.Context, identifier string) epochstypes.EpochInfo
 }
