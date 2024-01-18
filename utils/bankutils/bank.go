0f2a2d8d56c1fd09db83e633127edd6e94a4c906
Tuan Tran <tuantran@notional.ventures>
2024-01-18 21:29:24 +0700

    refactor: add bankutils as v0.46.16 bankkeeper

---
+++ utils/bankutils/bank.go
@@ -0,0 +1,97 @@
+package bankutils
+
+import (
+	"cosmossdk.io/math"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+	"github.com/cosmos/cosmos-sdk/types/kv"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
+// KVStore keys
+var (
+	SupplyKey           = []byte{0x00}
+	DenomMetadataPrefix = []byte{0x1}
+	DenomAddressPrefix  = []byte{0x03}
+
+	// BalancesPrefix is the prefix for the account balances store. We use a byte
+	// (instead of `[]byte("balances")` to save some disk space).
+	BalancesPrefix = []byte{0x02}
+)
+
+// AddressAndDenomFromBalancesStore returns an account address and denom from a balances prefix
+// store. The key must not contain the prefix BalancesPrefix as the prefix store
+// iterator discards the actual prefix.
+//
+// If invalid key is passed, AddressAndDenomFromBalancesStore returns ErrInvalidKey.
+func AddressAndDenomFromBalancesStore(key []byte) (sdk.AccAddress, string, error) {
+	if len(key) == 0 {
+		return nil, "", banktypes.ErrInvalidKey
+	}
+
+	kv.AssertKeyAtLeastLength(key, 1)
+
+	addrBound := int(key[0])
+
+	if len(key)-1 < addrBound {
+		return nil, "", banktypes.ErrInvalidKey
+	}
+
+	return key[1 : addrBound+1], string(key[addrBound+1:]), nil
+}
+
+// AddressFromBalancesStore returns an account address from a balances prefix
+// store. The key must not contain the prefix BalancesPrefix as the prefix store
+// iterator discards the actual prefix.
+//
+// If invalid key is passed, AddressFromBalancesStore returns ErrInvalidKey.
+func AddressFromBalancesStore(key []byte) (sdk.AccAddress, error) {
+	if len(key) == 0 {
+		return nil, banktypes.ErrInvalidKey
+	}
+
+	kv.AssertKeyAtLeastLength(key, 1)
+
+	addrLen := key[0]
+	bound := int(addrLen)
+
+	if len(key)-1 < bound {
+		return nil, banktypes.ErrInvalidKey
+	}
+
+	return key[1 : bound+1], nil
+}
+
+// CreateAccountBalancesPrefix creates the prefix for an account's balances.
+func CreateAccountBalancesPrefix(addr []byte) []byte {
+	return append(BalancesPrefix, address.MustLengthPrefix(addr)...)
+}
+
+// CreateDenomAddressPrefix creates a prefix for a reverse index of denomination
+// to account balance for that denomination.
+func CreateDenomAddressPrefix(denom string) []byte {
+	key := append(DenomAddressPrefix, []byte(denom)...)
+	return append(key, 0)
+}
+
+// Copied from https://github.com/cosmos/cosmos-sdk/blob/v0.46.16/x/bank/keeper/view.go#L243C1-L261
+// UnmarshalBalanceCompat unmarshal balance amount from storage, it's backward-compatible with the legacy format.
+func UnmarshalBalanceCompat(cdc codec.BinaryCodec, bz []byte, denom string) (sdk.Coin, error) {
+	amount := math.ZeroInt()
+	if bz == nil {
+		return sdk.NewCoin(denom, amount), nil
+	}
+
+	if err := amount.Unmarshal(bz); err != nil {
+		// try to unmarshal with the legacy format.
+		var balance sdk.Coin
+		if cdc.Unmarshal(bz, &balance) != nil {
+			// return with the original error
+			return sdk.Coin{}, err
+		}
+		return balance, nil
+	}
+
+	return sdk.NewCoin(denom, amount), nil
+}
