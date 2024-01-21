70ffbda3533d383d4cfa54debb5fa5ecd1fcc758
Tuan Tran <tuantran@notional.ventures>
2024-01-21 21:31:53 +0700

    fix: fix encoding format

--- x/interchainstaking/keeper/receipt.go
+++ x/interchainstaking/keeper/receipt.go
@@ -294,7 +294,7 @@ func (k *Keeper) SubmitTx(ctx sdk.Context, msgs []sdk.Msg, account *types.ICAAcc
 		msgs = msgs[chunkSize:]
 
 		// build and submit message for this chunk
-		data, err := icatypes.SerializeCosmosTx(k.cdc, msgsChunk, icatypes.EncodingProto3JSON)
+		data, err := icatypes.SerializeCosmosTx(k.cdc, msgsChunk, icatypes.EncodingProtobuf)
 		if err != nil {
 			return err
 		}
