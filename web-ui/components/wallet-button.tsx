a6cafc96d49753aa7290fcd0771d64127daedc02
Jacob Gadikian <jacobgadikian@gmail.com>
2024-01-22 16:56:29 +0000

    remove unused imports

--- web-ui/components/wallet-button.tsx
+++ web-ui/components/wallet-button.tsx
@@ -1,5 +1,5 @@
 import { Center, Grid, GridItem, Icon } from '@chakra-ui/react';
-import { useChain, useChains, useManager } from '@cosmos-kit/react';
+import { useChains } from '@cosmos-kit/react';
 import { MouseEventHandler } from 'react';
 import { FiAlertTriangle } from 'react-icons/fi';
 
