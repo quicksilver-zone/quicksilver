0feaade012139ac11e4a7ce7253392717a0005f8
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2024-01-25 00:25:45 -0700

    lint

--- web-ui/components/Governance/VotingSection.tsx
+++ web-ui/components/Governance/VotingSection.tsx
@@ -22,13 +22,14 @@ import { Proposal } from 'interchain-query/cosmos/gov/v1/gov';
 import React, { useMemo, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
+import { useVotingData } from '@/hooks';
+import { useGovernanceQuery } from '@/hooks/useQueries';
+import { decodeUint8Arr } from '@/utils';
+
 import { DisconnectedContent, Loader } from './common';
 import { ProposalCard } from './ProposalCard';
 import { ProposalModal } from './ProposalModal';
 
-import { useVotingData } from '@/hooks';
-import { useGovernanceQuery } from '@/hooks/useQueries';
-import { decodeUint8Arr } from '@/utils';
 
 function RotateIcon({ isOpen }: { isOpen: boolean }) {
   return (
