537c169943ed4a9fa960086a88fe2b04353a760c
Joe Bowman <joe@ingenuity.build>
2024-02-13 20:48:54 +0000

    add test for new hook behaviour

---
+++ x/participationrewards/keeper/hooks_test.go
@@ -0,0 +1,31 @@
+package keeper_test
+
+import (
+	icstypes "github.com/quicksilver-zone/quicksilver/x/interchainstaking/types"
+	"github.com/quicksilver-zone/quicksilver/x/participationrewards/types"
+)
+
+func (suite *KeeperTestSuite) TestAfterZoneCreated() {
+	zone := &icstypes.Zone{
+		ChainId:       "testzone-1",
+		ConnectionId:  "connection-0",
+		AccountPrefix: "test",
+		LocalDenom:    "uqtst",
+		BaseDenom:     "utst",
+	}
+	suite.Run("ProtocolData", func() {
+		k := suite.GetQuicksilverApp(suite.chainA).ParticipationRewardsKeeper
+		ctx := suite.chainA.GetContext()
+		suite.NoError(k.Hooks().AfterZoneCreated(ctx, zone))
+		ctx = suite.chainA.GetContext()
+		// we want to fetch this for the local (quicksilver) zone; not the host chain.
+		pd, found := k.GetProtocolData(ctx, types.ProtocolDataTypeLiquidToken, ctx.ChainID()+"_"+zone.LocalDenom)
+		suite.True(found)
+		upd, err := types.UnmarshalProtocolData(types.ProtocolDataTypeLiquidToken, pd.Data)
+		suite.NoError(err)
+		lpd := upd.(*types.LiquidAllowedDenomProtocolData)
+		suite.Equal(zone.ChainId, lpd.RegisteredZoneChainID)
+		suite.Equal(zone.LocalDenom, lpd.QAssetDenom)
+		suite.Equal(ctx.ChainID(), lpd.ChainID)
+	})
+}
