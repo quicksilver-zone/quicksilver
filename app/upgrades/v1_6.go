d222e1850890318e6417756c84724adc6809de7f
Joe Bowman <joe@ingenuity.build>
2024-03-06 13:53:56 +0400

    v1.5.0-rc1 upgrade handler (#1235)
    * refactor: upgrade handlers by version
    
    * v1.5.0-rc1 upgrade handler
    
    * lint

---
+++ app/upgrades/v1_6.go
@@ -0,0 +1,23 @@
+package upgrades
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/quicksilver-zone/quicksilver/app/keepers"
+)
+
+// =========== PRODUCTION UPGRADE HANDLER ===========
+
+func V010600UpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	appKeepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// no action yet.
+
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
