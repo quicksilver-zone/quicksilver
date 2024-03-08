57aeb9b51e5a6f0471e8a5791ca56d815956e7ff
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2024-03-07 20:55:13 -0700

    show no matches found for unmatched search term

--- web-ui/components/Staking/modals/validatorTable.tsx
+++ web-ui/components/Staking/modals/validatorTable.tsx
@@ -167,79 +167,72 @@ export const ValidatorsTable: React.FC<{
               </Tr>
             </Thead>
             <Tbody borderRadius={'10px'}>
-              {sortedValidators.length === 0
-                ? Array.from({ length: 5 }).map((_, index) => (
-                    <Tr key={index}>
-                      <Td border="1px solid rgba(255,128,0, 0.25)">
-                        <SkeletonCircle size="8" />
-                        <SkeletonText mt="4" noOfLines={1} spacing="4" />
+              {sortedValidators.length === 0 && searchTerm ? (
+                <Tr height="50px">
+                  {' '}
+                  <Td colSpan={3} textAlign="center" border="1px solid rgba(255,128,0, 0.25)" color="white">
+                    No matches found
+                  </Td>
+                </Tr>
+              ) : (
+                sortedValidators.map((validator, index) => {
+                  const votingPowerPercentage = totalVotingPower > 0 ? ((validator.votingPower || 0) / totalVotingPower) * 100 : 0;
+                  const validatorLogo = logos[validator.address];
+                  return (
+                    <Tr
+                      maxH={'50px'}
+                      cursor="pointer"
+                      key={index}
+                      _hover={{
+                        bgColor: 'rgba(255,128,0, 0.1)',
+                      }}
+                      onClick={() =>
+                        onValidatorClick({
+                          name: validator.name || '',
+                          operatorAddress: validator.address || '',
+                        })
+                      }
+                      backgroundColor={
+                        selectedValidators.some((v) => v.name === validator.name) ? 'rgba(255, 128, 0, 0.25)' : 'transparent'
+                      }
+                    >
+                      <Td maxW={'200px'} border="1px solid rgba(255,128,0, 0.25)" color="white">
+                        {!validatorLogo && (
+                          <SkeletonCircle
+                            boxSize="26px"
+                            objectFit="cover"
+                            marginRight="8px"
+                            display="inline-block"
+                            verticalAlign="middle"
+                            startColor="complimentary.900"
+                            endColor="complimentary.100"
+                          />
+                        )}
+                        {validatorLogo && (
+                          <Image
+                            borderRadius={'full'}
+                            src={validatorLogo}
+                            alt={validator.name}
+                            boxSize="26px"
+                            objectFit="cover"
+                            marginRight="8px"
+                            display="inline-block"
+                            verticalAlign="middle"
+                          />
+                        )}
+                        {(validator.name.length || 0) > 20 ? validator.name.substring(0, 14) + '...' : validator.name || ''}
                       </Td>
-                      <Td border="1px solid rgba(255,128,0, 0.25)">
-                        <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                      <Td border="1px solid rgba(255,128,0, 0.25)" color="white">
+                        {validator.commission ? validator.commission : 'N/A'}
                       </Td>
 
-                      <Td border="1px solid rgba(255,128,0, 0.25)">
-                        <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                      <Td border="1px solid rgba(255,128,0, 0.25)" color="white">
+                        {`${votingPowerPercentage.toFixed(2)}%`}
                       </Td>
                     </Tr>
-                  ))
-                : sortedValidators.map((validator, index) => {
-                    const votingPowerPercentage = totalVotingPower > 0 ? ((validator.votingPower || 0) / totalVotingPower) * 100 : 0;
-                    const validatorLogo = logos[validator.address];
-                    return (
-                      <Tr
-                        maxH={'50px'}
-                        cursor="pointer"
-                        key={index}
-                        _hover={{
-                          bgColor: 'rgba(255,128,0, 0.1)',
-                        }}
-                        onClick={() =>
-                          onValidatorClick({
-                            name: validator.name || '',
-                            operatorAddress: validator.address || '',
-                          })
-                        }
-                        backgroundColor={
-                          selectedValidators.some((v) => v.name === validator.name) ? 'rgba(255, 128, 0, 0.25)' : 'transparent'
-                        }
-                      >
-                        <Td maxW={'200px'} border="1px solid rgba(255,128,0, 0.25)" color="white">
-                          {!validatorLogo && (
-                            <SkeletonCircle
-                              boxSize="26px"
-                              objectFit="cover"
-                              marginRight="8px"
-                              display="inline-block"
-                              verticalAlign="middle"
-                              startColor="complimentary.900"
-                              endColor="complimentary.100"
-                            />
-                          )}
-                          {validatorLogo && (
-                            <Image
-                              borderRadius={'full'}
-                              src={validatorLogo}
-                              alt={validator.name}
-                              boxSize="26px"
-                              objectFit="cover"
-                              marginRight="8px"
-                              display="inline-block"
-                              verticalAlign="middle"
-                            />
-                          )}
-                          {(validator.name.length || 0) > 20 ? validator.name.substring(0, 14) || '' + '...' : validator.name || ''}
-                        </Td>
-                        <Td border="1px solid rgba(255,128,0, 0.25)" color="white">
-                          {validator.commission ? validator.commission : 'N/A'}
-                        </Td>
-
-                        <Td border="1px solid rgba(255,128,0, 0.25)" color="white">
-                          {`${votingPowerPercentage.toFixed(2)}%`}
-                        </Td>
-                      </Tr>
-                    );
-                  })}
+                  );
+                })
+              )}
             </Tbody>
           </Table>
         </TableContainer>
