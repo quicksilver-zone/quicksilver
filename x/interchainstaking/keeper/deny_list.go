e964ba68fe14f12928e728012632cfdad0d629b1
Tuan Tran <tuantran@notional.ventures>
2024-03-15 10:18:38 +0700

    refactor deny list storage
    Now store validator address bytes instead of serialized form of the Validator struct

--- x/interchainstaking/keeper/deny_list.go
+++ x/interchainstaking/keeper/deny_list.go
@@ -10,14 +10,17 @@ func (k *Keeper) SetZoneValidatorToDenyList(ctx sdk.Context, chainID string, val
 	store := ctx.KVStore(k.storeKey)
 
 	key := types.GetDeniedValidatorKey(chainID, validator.ValoperAddress)
-	b := types.MustMarshalValidator(k.cdc, validator)
-	store.Set(key, b)
+	addrBytes, err := sdk.ValAddressFromBech32(validator.ValoperAddress)
+	if err != nil {
+		return err
+	}
+	store.Set(key, addrBytes.Bytes())
 	return nil
 }
 
 // GetZoneValidatorDenyList get the validator deny list of a specific zone
-func (k *Keeper) GetZoneValidatorDenyList(ctx sdk.Context, chainID string) (denyList []types.Validator) {
-	k.IterateZoneDeniedValidator(ctx, chainID, func(validator types.Validator) bool {
+func (k *Keeper) GetZoneValidatorDenyList(ctx sdk.Context, chainID string) (denyList []string) {
+	k.IterateZoneDeniedValidator(ctx, chainID, func(validator string) bool {
 		denyList = append(denyList, validator)
 		return false
 	})
@@ -26,16 +29,19 @@ func (k *Keeper) GetZoneValidatorDenyList(ctx sdk.Context, chainID string) (deny
 }
 
 func (k *Keeper) GetDeniedValidatorInDenyList(ctx sdk.Context, chainID string, validatorAddress string) (types.Validator, bool) {
-	out := types.Validator{}
 	key := types.GetDeniedValidatorKey(chainID, validatorAddress)
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(key)
 	if bz == nil {
-		return out, false
+		return types.Validator{}, false
 	}
 
-	k.cdc.MustUnmarshal(bz, &out)
-	return out, true
+	val, found := k.GetValidator(ctx, chainID, bz)
+	if !found {
+		return types.Validator{}, false
+	}
+
+	return val, true
 
 }
 
@@ -46,7 +52,7 @@ func (k *Keeper) RemoveValidatorFromDenyList(ctx sdk.Context, chainID string, va
 	store.Delete(key)
 }
 
-func (k *Keeper) IterateZoneDeniedValidator(ctx sdk.Context, chainID string, cb func(validator types.Validator) (stop bool)) {
+func (k *Keeper) IterateZoneDeniedValidator(ctx sdk.Context, chainID string, cb func(validator string) (stop bool)) {
 	store := ctx.KVStore(k.storeKey)
 	deniedValPrefixKey := types.GetZoneDeniedValidatorKey(chainID)
 
@@ -54,7 +60,7 @@ func (k *Keeper) IterateZoneDeniedValidator(ctx sdk.Context, chainID string, cb 
 	defer iterator.Close()
 
 	for ; iterator.Valid(); iterator.Next() {
-		validator := types.MustUnmarshalValidator(k.cdc, iterator.Value())
+		validator := sdk.ValAddress(iterator.Value()).String()
 		if cb(validator) {
 			break
 		}
--- x/interchainstaking/keeper/deny_list_test.go
+++ x/interchainstaking/keeper/deny_list_test.go
@@ -1,10 +1,5 @@
 package keeper_test
 
-import (
-	"github.com/quicksilver-zone/quicksilver/utils/addressutils"
-	"github.com/quicksilver-zone/quicksilver/x/interchainstaking/types"
-)
-
 func (suite *KeeperTestSuite) TestStoreGetDeleteDenyList() {
 	suite.Run("deny list - store / get / delete single", func() {
 		suite.SetupTest()
@@ -15,30 +10,30 @@ func (suite *KeeperTestSuite) TestStoreGetDeleteDenyList() {
 
 		zone, found := qApp.InterchainstakingKeeper.GetZone(ctx, suite.chainB.ChainID)
 		suite.True(found)
-		validator := addressutils.GenerateValAddressForTest()
-
+		vals := qApp.InterchainstakingKeeper.GetValidators(ctx, zone.ChainId)
+		suite.Len(vals, 4)
+		val := vals[0]
+		validator := vals[0].ValoperAddress
 		// Initially the deny list should be empty
-		_, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, validator.String())
+		_, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, validator)
 		suite.False(found)
 		// Add a validator to the deny list
-		err := qApp.InterchainstakingKeeper.SetZoneValidatorToDenyList(ctx, zone.ChainId, types.Validator{
-			ValoperAddress: validator.String(),
-		})
+		err := qApp.InterchainstakingKeeper.SetZoneValidatorToDenyList(ctx, zone.ChainId, val)
 		suite.NoError(err)
 
 		// Ensure the deny list contains the validator
 
 		denyList := qApp.InterchainstakingKeeper.GetZoneValidatorDenyList(ctx, zone.ChainId)
 		suite.Len(denyList, 1)
-		suite.Equal(validator.String(), denyList[0].ValoperAddress)
+		suite.Equal(validator, denyList[0])
 
-		val, found := qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, validator.String())
+		val, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, validator)
 		suite.True(found)
-		suite.Equal(validator.String(), val.ValoperAddress)
+		suite.Equal(validator, val.ValoperAddress)
 
 		// Remove the validator from the deny list
 		qApp.InterchainstakingKeeper.RemoveValidatorFromDenyList(ctx, zone.ChainId, val)
-		_, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, validator.String())
+		_, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, validator)
 		suite.False(found)
 
 		denyList = qApp.InterchainstakingKeeper.GetZoneValidatorDenyList(ctx, zone.ChainId)
@@ -54,52 +49,56 @@ func (suite *KeeperTestSuite) TestStoreGetDeleteDenyList() {
 
 		zone, found := qApp.InterchainstakingKeeper.GetZone(ctx, suite.chainB.ChainID)
 		suite.True(found)
-
-		valAddr1 := addressutils.GenerateValAddressForTest()
-		valAddr2 := addressutils.GenerateValAddressForTest()
-		valAddr3 := addressutils.GenerateValAddressForTest()
+		vals := qApp.InterchainstakingKeeper.GetValidators(ctx, zone.ChainId)
+		suite.Len(vals, 4)
+		val1, val2, val3 := vals[0], vals[1], vals[2]
+		valAddr1, valAddr2, valAddr3 := val1.ValoperAddress, val2.ValoperAddress, val3.ValoperAddress
 
 		// Initially the deny list should be empty
-		_, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr1.String())
+		_, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr1)
 		suite.False(found)
 
 		// Add three validators to the deny list
-		err := qApp.InterchainstakingKeeper.SetZoneValidatorToDenyList(ctx, zone.ChainId, types.Validator{
-			ValoperAddress: valAddr1.String(),
-		})
+		err := qApp.InterchainstakingKeeper.SetZoneValidatorToDenyList(ctx, zone.ChainId, val1)
 		suite.NoError(err)
-		err = qApp.InterchainstakingKeeper.SetZoneValidatorToDenyList(ctx, zone.ChainId, types.Validator{
-			ValoperAddress: valAddr2.String(),
-		})
+		err = qApp.InterchainstakingKeeper.SetZoneValidatorToDenyList(ctx, zone.ChainId, val2)
 		suite.NoError(err)
-		err = qApp.InterchainstakingKeeper.SetZoneValidatorToDenyList(ctx, zone.ChainId, types.Validator{
-			ValoperAddress: valAddr3.String(),
-		})
+		err = qApp.InterchainstakingKeeper.SetZoneValidatorToDenyList(ctx, zone.ChainId, val3)
 		suite.NoError(err)
 
-		// Ensure the deny list contains the three validators
 		denyList := qApp.InterchainstakingKeeper.GetZoneValidatorDenyList(ctx, zone.ChainId)
 		suite.Len(denyList, 3)
 
-		val1, found := qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr1.String())
+		denyVal1, found := qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr1)
 		suite.True(found)
-		suite.Equal(valAddr1.String(), val1.ValoperAddress)
+		suite.Equal(valAddr1, denyVal1.ValoperAddress)
 
-		val2, found := qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr2.String())
+		denyVal2, found := qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr2)
 		suite.True(found)
-		suite.Equal(valAddr2.String(), val2.ValoperAddress)
+		suite.Equal(valAddr2, denyVal2.ValoperAddress)
 
-		val3, found := qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr3.String())
+		denyVal3, found := qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr3)
 		suite.True(found)
-		suite.Equal(valAddr3.String(), val3.ValoperAddress)
+		suite.Equal(valAddr3, denyVal3.ValoperAddress)
 
 		// Remove the validator from the deny list
 		qApp.InterchainstakingKeeper.RemoveValidatorFromDenyList(ctx, zone.ChainId, val2)
-		_, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, val2.String())
+		_, found = qApp.InterchainstakingKeeper.GetDeniedValidatorInDenyList(ctx, zone.ChainId, valAddr2)
 		suite.False(found)
 
 		// Ensure the deny list contains the two remaining validators
 		denyList = qApp.InterchainstakingKeeper.GetZoneValidatorDenyList(ctx, zone.ChainId)
 		suite.Len(denyList, 2)
+		suite.NotContains(denyList, valAddr2)
+		suite.Contains(denyList, valAddr1)
+		suite.Contains(denyList, valAddr3)
+
+		// Remove the remaining two validators from the deny list
+		qApp.InterchainstakingKeeper.RemoveValidatorFromDenyList(ctx, zone.ChainId, val1)
+		qApp.InterchainstakingKeeper.RemoveValidatorFromDenyList(ctx, zone.ChainId, val3)
+
+		// Ensure the deny list is empty
+		denyList = qApp.InterchainstakingKeeper.GetZoneValidatorDenyList(ctx, zone.ChainId)
+		suite.Len(denyList, 0)
 	})
 }
