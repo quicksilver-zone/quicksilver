5872d2c191f7539098aee3ea27e6ba6406df445f
Joe Bowman <joe@ingenuity.build>
2024-03-19 11:47:47 +0000

    fix nil reference

--- x/interchainstaking/keeper/zones.go
+++ x/interchainstaking/keeper/zones.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 
+	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/bech32"
@@ -436,6 +437,7 @@ OUTER:
 func (k *Keeper) CollectStatsForZone(ctx sdk.Context, zone *types.Zone) (*types.Statistics, error) {
 	out := &types.Statistics{}
 	out.ChainId = zone.ChainId
+	out.Deposited = sdkmath.ZeroInt()
 	out.Delegated = k.GetDelegatedAmount(ctx, zone).Amount
 	userMap := map[string]bool{}
 	k.IterateZoneReceipts(ctx, zone.ChainId, func(_ int64, receipt types.Receipt) bool {
