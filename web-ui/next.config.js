33551d7e7d07d63ac1146035ee1155642c9c1335
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2024-03-26 09:48:11 -0700

    update lockfile

--- web-ui/next.config.js
+++ web-ui/next.config.js
@@ -14,9 +14,3 @@ module.exports = {
     ];
   },
 };
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer({});
