c629300caf8ae2e07cb67737da9f913f1f7d5f56
minhngoc274 <ngoc@notional.ventures>
2024-04-12 02:39:14 +0700

    fix test and bump pfm v6

--- x/interchainstaking/keeper/receipt.go
+++ x/interchainstaking/keeper/receipt.go
@@ -3,6 +3,8 @@ package keeper
 import (
 	"errors"
 	"fmt"
+	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	"github.com/gogo/protobuf/proto"
 	"time"
 
 	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
