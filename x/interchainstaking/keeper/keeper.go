b93808c6186e3e69028b90c3d8fb2767994fa545
minhngoc274 <ngoc@notional.ventures>
2024-04-11 14:08:52 +0700

    refactor: change hardcode into config

--- x/interchainstaking/keeper/keeper.go
+++ x/interchainstaking/keeper/keeper.go
@@ -784,8 +784,7 @@ func (k *Keeper) Rebalance(ctx sdk.Context, zone *types.Zone, epochNumber int64)
 	rebalances := types.DetermineAllocationsForRebalancing(currentAllocations, currentLocked, currentSum, lockedSum, targetAllocations, maxCanAllocate, k.Logger(ctx)).RemoveDuplicates()
 	msgs := make([]sdk.Msg, 0)
 	for _, rebalance := range rebalances {
-		if rebalance.Amount.GTE(sdk.NewInt(1_000_000)) {
-			// don't redelegate dust; TODO: config per zone
+		if rebalance.Amount.GTE(sdk.NewInt(zone.DustThreshold)) {
 			if !rebalance.Amount.IsInt64() {
 				k.Logger(ctx).Error("Rebalance amount out of bound Int64", "amount", rebalance.Amount.String())
 				// Ignore this
