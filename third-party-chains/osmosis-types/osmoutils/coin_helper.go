afb1ea0e86d1a874c326ea1835af6404c2cb5432
Joe Bowman <joe@ingenuity.build>
2024-04-30 14:49:54 +0100

    remove wasm references from osmosis-types

--- third-party-chains/osmosis-types/osmoutils/coin_helper.go
+++ third-party-chains/osmosis-types/osmoutils/coin_helper.go
@@ -3,7 +3,6 @@ package osmoutils
 import (
 	"fmt"
 
-	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -101,23 +100,6 @@ func MergeCoinMaps[T comparable](currentEpochExpectedDistributionsOne map[T]sdk.
 		}
 	}
 	return newMap
-}
-
-// Convert sdk.Coins to wasmvmtypes.Coins
-func CWCoinsFromSDKCoins(in sdk.Coins) wasmvmtypes.Coins {
-	var cwCoins wasmvmtypes.Coins
-	for _, coin := range in {
-		cwCoins = append(cwCoins, CWCoinFromSDKCoin(coin))
-	}
-	return cwCoins
-}
-
-// Convert sdk.Coin to wasmvmtypes.Coin
-func CWCoinFromSDKCoin(in sdk.Coin) wasmvmtypes.Coin {
-	return wasmvmtypes.Coin{
-		Denom:  in.GetDenom(),
-		Amount: in.Amount.String(),
-	}
 }
 
 func ConvertCoinArrayToCoins(coinArray []sdk.Coin) sdk.Coins {
--- third-party-chains/osmosis-types/osmoutils/ibc.go
+++ third-party-chains/osmosis-types/osmoutils/ibc.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
-	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
-	ibcexported "github.com/cosmos/ibc-go/v5/modules/core/exported"
+	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	ibcexported "github.com/cosmos/ibc-go/v6/modules/core/exported"
 )
 
 const IbcAcknowledgementErrorType = "ibc-acknowledgement-error"
