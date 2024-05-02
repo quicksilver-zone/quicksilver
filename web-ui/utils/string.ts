659a160f73e4e3575c47d2473ada534ac66c56a2
tiendn <tiendn.works@gmail.com>
2024-05-02 21:58:40 +0700

    style: update space for enhance the readability and maintainability of the code

--- web-ui/utils/string.ts
+++ web-ui/utils/string.ts
@@ -1,6 +1,6 @@
   export function formatQasset(denom: string): string {
     if (denom.startsWith("Q") || denom.startsWith("AQ")){
-        return "q"+denom.substring(1)
+        return "q" + denom.substring(1)
       } 
       return denom
   }
