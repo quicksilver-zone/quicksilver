ebf862aac9d3083640dcef182ea403d1691d9be0
tiendn <tiendn.works@gmail.com>
2024-05-02 21:50:02 +0700

    fix: issue 1381 using startWiths instead of substring for check string prefixess

--- web-ui/utils/string.ts
+++ web-ui/utils/string.ts
@@ -1,5 +1,5 @@
   export function formatQasset(denom: string): string {
-    if (denom.substring(0, 1) == "Q" || denom.substring(0, 2) == "AQ"){
+    if (denom.startsWith("Q") || denom.startsWith("AQ")){
         return "q"+denom.substring(1)
       } 
       return denom
