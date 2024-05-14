ecb6847eb155ceb5e4af0ea9bdc876100d0c1ec7
Joe Bowman <joe@ingenuity.build>
2024-05-14 11:10:57 +0100

    add osmosis cl type to loaded modules

--- x/participationrewards/keeper/keeper.go
+++ x/participationrewards/keeper/keeper.go
@@ -173,6 +173,7 @@ func LoadSubmodules() map[cmtypes.ClaimType]Submodule {
 	out := make(map[cmtypes.ClaimType]Submodule, 0)
 	out[cmtypes.ClaimTypeLiquidToken] = &LiquidTokensModule{}
 	out[cmtypes.ClaimTypeOsmosisPool] = &OsmosisModule{}
+	out[cmtypes.ClaimTypeOsmosisCLPool] = &OsmosisClModule{}
 	out[cmtypes.ClaimTypeUmeeToken] = &UmeeModule{}
 	return out
 }
