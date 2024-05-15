9c797cc019c2d7401376a3c23f9e4c6de0a9fdac
tiendn <tiendn.works@gmail.com>
2024-05-15 09:12:38 +0700

    fix: remove dup code

--- web-ui/utils/string.ts
+++ web-ui/utils/string.ts
@@ -1,9 +1,7 @@
   export function formatQasset(denom: string): string {
-    if (denom.startsWith("Q") || denom.startsWith("AQ")) {
-        return "q" + denom.substring(1);
-    } 
+    if (denom.startsWith('Q') || denom.startsWith('AQ')) {
+      return 'q' + denom.substring(1);
+    }
+
     return denom;
-        return "q" + denom.substring(1)
-      } 
-      return denom
   }
