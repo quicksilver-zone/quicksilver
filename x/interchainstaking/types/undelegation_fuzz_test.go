b88d904cc424ee960bb26b6f9d7d38588b4e31a4
Tuan Tran <tuantran@notional.ventures>
2024-05-28 14:42:52 +0700

    remove predef

--- x/interchainstaking/types/undelegation_fuzz_test.go
+++ x/interchainstaking/types/undelegation_fuzz_test.go
@@ -34,31 +34,3 @@ func FuzzDetermineAllocationsForUndelegation(f *testing.F) {
 			drt.Amount)
 	})
 }
-
-func FuzzDetermineAllocationsForUndelegationPredef(f *testing.F) {
-	if testing.Short() {
-		f.Skip("Running in -short mode")
-	}
-
-	seeds := detRedemptionTests(vals)
-	for _, seed := range seeds {
-		blob, err := json.Marshal(seed)
-		if err == nil {
-			f.Add(blob)
-		}
-	}
-
-	f.Fuzz(func(_ *testing.T, inputJSON []byte) {
-		drt := new(detRedemptionTest)
-		if err := json.Unmarshal(inputJSON, drt); err != nil {
-			return
-		}
-		_, _ = types.DetermineAllocationsForUndelegationPredef(
-			drt.CurrentAllocations,
-			map[string]bool{},
-			sum(drt.CurrentAllocations),
-			drt.TargetAllocations,
-			drt.Unlocked,
-			drt.Amount)
-	})
-}
