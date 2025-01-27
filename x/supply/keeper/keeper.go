3e514972015b697d097dc324df3ebc744c2a2234
Joe Bowman <joe@quicksilver.zone>
2025-01-27 18:08:49 +0000

    lint

--- x/supply/keeper/keeper.go
+++ x/supply/keeper/keeper.go
@@ -39,7 +39,6 @@ func NewKeeper(
 	moduleAccounts []string,
 	endpointEnabled bool,
 ) Keeper {
-
 	if addr := ak.GetModuleAddress(types.AirdropAccount); addr == nil {
 		panic(fmt.Sprintf("%s module account has not been set", types.AirdropAccount))
 	}
