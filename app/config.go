6f6c742b470130521b23e03eb3a5bc61e7603b88
Joe Bowman <joe@ingenuity.build>
2025-03-27 14:22:51 +0000

    fix TestIntegrationTestSuite appconstructor

--- app/config.go
+++ app/config.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"time"
 
+	dbm "github.com/cometbft/cometbft-db"
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	"github.com/cosmos/cosmos-sdk/testutil/network"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
@@ -22,39 +25,40 @@ func DefaultConfig() network.Config {
 		LegacyAmino:       encCfg.Amino,
 		InterfaceRegistry: encCfg.InterfaceRegistry,
 		AccountRetriever:  authtypes.AccountRetriever{},
-		// AppConstructor:    NewAppConstructor(encCfg),
-		GenesisState:   ModuleBasics.DefaultGenesis(encCfg.Marshaler),
-		TimeoutCommit:  1 * time.Second / 2,
-		ChainID:        "quicktest-1",
-		NumValidators:  1,
-		BondDenom:      sdk.DefaultBondDenom,
-		MinGasPrices:   fmt.Sprintf("0.000006%s", sdk.DefaultBondDenom),
-		AccountTokens:  sdk.TokensFromConsensusPower(1000, sdk.DefaultPowerReduction),
-		StakingTokens:  sdk.TokensFromConsensusPower(500, sdk.DefaultPowerReduction),
-		BondedTokens:   sdk.TokensFromConsensusPower(100, sdk.DefaultPowerReduction),
-		CleanupDir:     true,
-		SigningAlgo:    string(hd.Secp256k1Type),
-		KeyringOptions: []keyring.Option{},
+		AppConstructor:    NewAppConstructor(encCfg),
+		GenesisState:      ModuleBasics.DefaultGenesis(encCfg.Marshaler),
+		TimeoutCommit:     1 * time.Second / 2,
+		ChainID:           "quicksilver-1",
+		NumValidators:     1,
+		BondDenom:         sdk.DefaultBondDenom,
+		MinGasPrices:      fmt.Sprintf("0.000006%s", sdk.DefaultBondDenom),
+		AccountTokens:     sdk.TokensFromConsensusPower(1000, sdk.DefaultPowerReduction),
+		StakingTokens:     sdk.TokensFromConsensusPower(500, sdk.DefaultPowerReduction),
+		BondedTokens:      sdk.TokensFromConsensusPower(100, sdk.DefaultPowerReduction),
+		CleanupDir:        true,
+		SigningAlgo:       string(hd.Secp256k1Type),
+		KeyringOptions:    []keyring.Option{},
 	}
 }
 
-// func NewAppConstructor(encCfg EncodingConfig) network.AppConstructor {
-// 	return func(val network.Validator) servertypes.Application {
-// 		return NewQuicksilver(
-// 			val.Ctx.Logger,
-// 			dbm.NewMemDB(),
-// 			nil,
-// 			true,
-// 			map[int64]bool{},
-// 			DefaultNodeHome,
-// 			0,
-// 			encCfg,
-// 			EmptyAppOptions{},
-// 			false,
-// 			false,
-// 			"",
-// 			//baseapp.SetPruning(purningtypes.NewPruningOptionsFromString(val.AppConfig.Pruning)),
-// 			// baseapp.SetMinGasPrices(val.AppConfig.MinGasPrices),
-// 		)
-// 	}
-// }
+func NewAppConstructor(encCfg EncodingConfig) network.AppConstructor {
+	return func(val network.ValidatorI) servertypes.Application {
+		return NewQuicksilver(
+			val.GetCtx().Logger,
+			dbm.NewMemDB(),
+			nil,
+			true,
+			map[int64]bool{},
+			DefaultNodeHome,
+			0,
+			encCfg,
+			EmptyAppOptions{},
+			false,
+			false,
+			"",
+			//baseapp.SetPruning(purningtypes.NewPruningOptionsFromString(val.AppConfig.Pruning)),
+			// baseapp.SetMinGasPrices(val.AppConfig.MinGasPrices),
+			baseapp.SetChainID("quicksilver-1"),
+		)
+	}
+}
