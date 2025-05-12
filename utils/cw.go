30052543bac25d3069ed348dd3aaaa2ee9a2391e
Joe Bowman <joe@quicksilver.zone>
2025-05-12 15:39:09 +0100

    add docs for DecodeCwNamespacedKey

--- utils/cw.go
+++ utils/cw.go
@@ -6,6 +6,15 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// DecodeCwNamespacedKey decodes a cw namespaced key into an address and parts.
+// The key is expected to be in the format of a cw namespaced key, which is
+// a prefix (1 byte), a 32 byte address, a null terminator (1 byte), 1..n parts
+// (each part is a length-prefixed byte string), followed by the final key (not
+// length-prefixed).
+// As specified here: https://github.com/webmaster128/key-namespacing#nesting
+//
+// As we are unable to determine the length of the final key, we must pass in the
+// number of parts we expect out (including the final key).
 func DecodeCwNamespacedKey(key []byte, numParts int) (sdk.AccAddress, [][]byte, error) {
 
 	if len(key) < 37 { // prefix (1 byte) + 32 byte address + 1 byte null terminator + len (1 byte) + min 1 byte namespace + min 1 byte key
