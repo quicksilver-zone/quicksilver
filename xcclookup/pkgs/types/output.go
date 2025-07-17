f7f97d204f98f96ea6d9aa0fcef26751066bce57
Joe Bowman <joe@quicksilver.zone>
2025-07-17 09:34:34 +0100

    refactor output helpers

--- xcclookup/pkgs/types/output.go
+++ xcclookup/pkgs/types/output.go
@@ -8,37 +8,27 @@ import (
 	"github.com/ingenuity-build/multierror"
 )
 
-var OutputEpoch = func(w http.ResponseWriter, response *Response, errors map[string]error) {
-	fmt.Println("check for errors...")
+func outputResponse(w http.ResponseWriter, response *Response, errors map[string]error, clearMessages bool) {
 	if len(errors) > 0 {
-		fmt.Printf("found %d error(s)\n", len(errors))
 		response.Errors = multierror.New(errors)
-		fmt.Println(response.Errors)
+	}
+
+	if clearMessages {
+		response.Messages = nil
 	}
 
 	jsonOut, err := json.Marshal(response)
 	if err != nil {
-		fmt.Println("Error:", err)
 		fmt.Fprintf(w, "Error: %s", err.Error())
 		return
 	}
 	fmt.Fprint(w, string(jsonOut))
 }
 
-var OutputCurrent = func(w http.ResponseWriter, response *Response, errors map[string]error) {
-	fmt.Println("check for errors...")
-	if len(errors) > 0 {
-		fmt.Printf("found %d error(s)\n", len(errors))
-		response.Errors = multierror.New(errors)
-		fmt.Println(response.Errors)
-	}
+var OutputEpoch = func(w http.ResponseWriter, response *Response, errors map[string]error) {
+	outputResponse(w, response, errors, false)
+}
 
-	response.Messages = nil
-	jsonOut, err := json.Marshal(response)
-	if err != nil {
-		fmt.Println("Error:", err)
-		fmt.Fprintf(w, "Error: %s", err)
-		return
-	}
-	fmt.Fprint(w, string(jsonOut))
+var OutputCurrent = func(w http.ResponseWriter, response *Response, errors map[string]error) {
+	outputResponse(w, response, errors, true)
 }
