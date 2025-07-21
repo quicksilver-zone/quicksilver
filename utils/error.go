2e669a264bac487b3ab12708e82cd9aef037c76f
Joe Bowman <joe@quicksilver.zone>
2025-07-21 10:38:28 +0100

    preallocate errors slice

--- utils/error.go
+++ utils/error.go
@@ -6,7 +6,7 @@ import (
 
 // helper function to convert error map to slice for multierr
 func ErrorMapToSlice(errs map[string]error) []error {
-	var errList []error
+	errList := make([]error, 0, len(errs)) // pre-allocate memory for the slice
 	for _, err := range Keys(errs) {
 		errList = append(errList, fmt.Errorf("%s: %w", err, errs[err]))
 	}
